7e156d0e8190a271186b8dfe61fe1642f4d6b518
Frenco <hey@frenco.dev>
2022-11-01 12:11:49 +0630

    ci: update docs site to run on local as well

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/workbench-design',
+  basePath: process.env.BASE_PATH || '/',
   images: {
     unoptimized: true,
   },
