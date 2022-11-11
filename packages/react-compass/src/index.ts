789d5b3c838c1854a4d6fbddf4b4cb33f7571afa
Tee Nguyen <tungnt107@fsoft.com.vn>
2022-11-11 15:33:56 +0700

    feat: add typesVersions for build, support import from sub-directory

--- packages/react-compass/src/index.ts
+++ packages/react-compass/src/index.ts
@@ -1,9 +1,13 @@
-export {default as Alert} from './alert'
-export {default as Avatar} from './avatar'
-export {default as Badge} from './badge'
-export {default as Breadcrumbs} from './breadcrumbs'
-export {default as Button} from './button'
-export {default as Link} from './link'
+export {AlertProps, default as Alert} from './alert'
+export {AvatarGroupProps, AvatarProps, default as Avatar} from './avatar'
+export {BadgeProps, default as Badge} from './badge'
+export {
+  BreadcrumbItemProps,
+  BreadcrumbsProps,
+  default as Breadcrumbs,
+} from './breadcrumbs'
+export {ButtonProps, default as Button} from './button'
+export {default as Link, LinkProps} from './link'
 export {default as Preflight} from './preflight'
-export {default as Spinner} from './spinner'
+export {default as Spinner, SpinnerProps} from './spinner'
 export {globalCss, styled, theme} from './theme'
