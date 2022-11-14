5d8ebbb946554383d65c28cb891612a47fe5fb7e
michael-le-19 <minhlt19@fsoft.com.vn>
2022-11-14 16:01:13 +0700

    write card docs & refactor style naming

--- apps/docs/src/components/ComponentDisplay.tsx
+++ apps/docs/src/components/ComponentDisplay.tsx
@@ -3,7 +3,7 @@ import type {PropsWithChildren} from 'react'
 interface Props {}
 
 const ComponentDisplay: React.FC<PropsWithChildren<Props>> = ({children}) => {
-  return <div className='isolated bg-red-400'>{children}</div>
+  return <div className='isolated bg-gray-100 p-6'>{children}</div>
 }
 
 export default ComponentDisplay
--- packages/react-compass/src/card/cardTitle.styles.ts
+++ packages/react-compass/src/card/cardTitle.styles.ts
@@ -1,13 +1,13 @@
 import {styled} from '../theme'
 
-export const StyledBCardTitle = styled('div', {
+export const StyledCardTitleContainer = styled('div', {
   width: '100%',
   position: 'relative',
   backgroundColor: 'inherit',
+  marginBottom: '12px',
+})
 
-  '& .card-default-title': {
-    fontSize: '$header4',
-    margin: 0,
-    marginBottom: '12px',
-  },
+export const StyledCardTitle = styled('h1', {
+  fontSize: '$header4',
+  margin: 0,
 })
