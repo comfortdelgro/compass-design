d8459bf6c26cbc3239959e67b7a54c6a2c043f7b
michael-le-19 <minhlt19@fsoft.com.vn>
2022-12-06 15:15:46 +0700

    add some apis, fix visual bugs

--- packages/react-compass/src/card/index.ts
+++ packages/react-compass/src/card/index.ts
@@ -1,9 +1,11 @@
 import Card from './card'
+import CardAction from './card-action'
 import CardBody from './card-body'
 import CardImage from './card-image'
 import CardTitle from './card-title'
 
 export type {CardProps} from './card'
+export type {CardActionProps} from './card-action'
 export type {CardBodyProps} from './card-body'
 export type {CardImageProps} from './card-image'
 export type {CardTitleProps} from './card-title'
@@ -11,5 +13,6 @@ export type {CardTitleProps} from './card-title'
 Card.Body = CardBody
 Card.Image = CardImage
 Card.Title = CardTitle
+Card.Action = CardAction
 
 export default Card
