8acb33d1e373610420835c7ef11a97f27917a48c
Tee Nguyen <tungnt107@fsoft.com.vn>
2022-12-22 10:27:17 +0700

    feat: added testing for components

---
+++ packages/react-compass/src/menu-list/menu-list.cy.tsx
@@ -0,0 +1,43 @@
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import MenuList, {MenuListDropdown} from './index'
+
+describe('menu-list.cy.ts', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+  })
+  it('C01: List should render children', () => {
+    const menuList = (
+      <MenuList css={{height: '800px'}} id='menu'>
+        <MenuListDropdown>
+          <MenuListDropdown.Header>Dashboard</MenuListDropdown.Header>
+          <MenuListDropdown.Item>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item isActive>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item>Menu</MenuListDropdown.Item>
+        </MenuListDropdown>
+        <MenuListDropdown>
+          <MenuListDropdown.Header leftIcon={false} rightIcon={true}>
+            More menu
+          </MenuListDropdown.Header>
+          <MenuListDropdown.Item icon={faUser}>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item icon={faUser}>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item icon={faUser}>Menu</MenuListDropdown.Item>
+          <MenuListDropdown.Item isNested>
+            <MenuListDropdown>
+              <MenuListDropdown.Header>Nested menu</MenuListDropdown.Header>
+              <MenuListDropdown.Item icon={faUser}>Menu</MenuListDropdown.Item>
+              <MenuListDropdown.Item icon={faUser}>Menu</MenuListDropdown.Item>
+              <MenuListDropdown.Item icon={faUser}>Menu</MenuListDropdown.Item>
+              <MenuListDropdown.Item icon={faUser}>Menu</MenuListDropdown.Item>
+            </MenuListDropdown>
+          </MenuListDropdown.Item>
+        </MenuListDropdown>
+      </MenuList>
+    )
+    cy.mount(menuList)
+    cy.get('#menu').children().should('be.visible')
+  })
+})
