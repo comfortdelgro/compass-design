f81926a40dd6837590997643451a9a992e7f5b42
Tai Bui <tai.c.bui@gmail.com>
2022-12-25 23:15:50 +0700

    Finish pagination

---
+++ packages/react-compass/src/pagination/pagination.cy.tsx
@@ -0,0 +1,27 @@
+import Pagination from './index'
+
+describe('pagination.cy.ts', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+  })
+
+  it('C01: ', () => {
+    const pagination = (
+      <Pagination
+        id='pagination'
+        total={10}
+        page={2}
+        onChange={(page) => {
+          console.log(page)
+        }}
+      />
+    )
+
+    cy.mount(pagination)
+    //confirm total number of pages shown
+    cy.get('.c-hSXQHM').and('contain', '10')
+
+    //confirm current page selected
+    cy.get('.c-hSXQHM-hJZsla-active-true').should('have.text', '2')
+  })
+})
