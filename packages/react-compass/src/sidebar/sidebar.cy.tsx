2e1d9221bb243168aae940ca2f6334bb07fb8757
Tai Bui <tai.c.bui@gmail.com>
2022-12-26 21:27:39 +0700

    Finish

---
+++ packages/react-compass/src/sidebar/sidebar.cy.tsx
@@ -0,0 +1,15 @@
+import {Default as Sidebar} from './sidebar.stories'
+
+describe('sidebar.cy.ts', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+  })
+
+  it('C01: confirm sidebar popup when button is clicked', () => {
+    cy.mount(<Sidebar />)
+    cy.get('.c-jdhZyR').click()
+    cy.get('.c-bstbuK').should('have.css', 'display').and('eq', 'block')
+    cy.get('svg').click()
+    cy.get('.c-bstbuK').should('not.exist')
+  })
+})
