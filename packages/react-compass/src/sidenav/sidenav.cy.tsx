79827953dabc7f4b3e7681c150c2e1a5a2ce9f3b
Tai Bui <75408677+Tai-Chi-Bui@users.noreply.github.com>
2022-12-26 23:12:10 +0700

    chore/add-test-and-docs-to-Sidenav (#194)

---
+++ packages/react-compass/src/sidenav/sidenav.cy.tsx
@@ -0,0 +1,15 @@
+import {NoExpandSidenav} from './testComponent'
+
+describe('range-calendar.cy.ts', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+  })
+
+  const nav = <NoExpandSidenav />
+
+  it('C01: confirm date selected', () => {
+    cy.mount(nav)
+    cy.get('.c-TnYYI').trigger('mouseover')
+    cy.get('.sidenav-expanded').should('exist')
+  })
+})
