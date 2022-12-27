557e1fc3041ffe347338b411505604c882c39b55
Tai Bui <tai.c.bui@gmail.com>
2022-12-28 00:07:18 +0700

    finish wizard test + docs

---
+++ packages/react-compass/src/wizard/wizard.cy.tsx
@@ -0,0 +1,17 @@
+import {Variants as Wizard} from './wizard.stories'
+
+describe('wizard.cy.ts', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+  })
+
+  it('C01: confirm the wizard shows correct current step', () => {
+    cy.mount(<Wizard />)
+    cy.get('.wizard-progress-wrapper > :nth-child(1) > div')
+      .should('have.attr', 'class')
+      .and('include', 'c-hGkPwH-ftQEAz-active-true')
+    cy.get('.wizard-progress-wrapper > :nth-child(2) > div')
+      .should('have.attr', 'class')
+      .and('include', 'c-hGkPwH-ftQEAz-active-true')
+  })
+})
