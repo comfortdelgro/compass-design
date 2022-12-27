ef5b4da7c7c603af5f972dd1ff02df05cf693ac7
Tai Bui <75408677+Tai-Chi-Bui@users.noreply.github.com>
2022-12-27 23:21:58 +0700

    chore: write test + docs for toggle (#206)

---
+++ packages/react-compass/src/toggle/toggle.cy.tsx
@@ -0,0 +1,15 @@
+import {Variants as Toggles} from './toggle.stories'
+
+describe('toggle.cy.ts', () => {
+  beforeEach(() => {
+    cy.viewport(1280, 720)
+  })
+
+  it('C01: confirm toggle switch when being clicked', () => {
+    cy.mount(<Toggles />)
+    cy.get('.c-iTKOFX > :nth-child(2) > :nth-child(1)').click()
+    cy.get('.c-iTKOFX > :nth-child(2) > :nth-child(1)')
+      .should('have.attr', 'class')
+      .and('include', 'c-jozUte-kBgkqj-active-true')
+  })
+})
