8de9dd9a7a296dd79efc2d3231f2a85fc6c4a005
Tai Bui <75408677+Tai-Chi-Bui@users.noreply.github.com>
2023-01-12 11:07:28 +0700

    Feature/grid component (#253)
    * feature/dashboarad-sidecard
    
    * feature/grid-component

--- packages/react-compass/src/theme/breakpoints.ts
+++ packages/react-compass/src/theme/breakpoints.ts
@@ -1,6 +1,10 @@
 const breakpoints = {
   tablet: '(max-width: 768px)',
   mobile: '(max-width: 480px)',
+  xs: '(max-width: 480px)',
+  sm: '(max-width: 640px)',
+  md: '(max-width: 768px)',
+  lg: '(max-width: 1024px)',
 }
 
 export default breakpoints
