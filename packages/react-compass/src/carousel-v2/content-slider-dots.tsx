5c6e983fcf816cedb9afcd543d7d04ab09916196
fpt-duongtx <119040724+fpt-duongtx@users.noreply.github.com>
2023-02-05 22:10:49 +0700

    feature: Restructure / rename carousel module (#308)
    * Restructure carousel module
    
    * Adding delegated to components
    
    * Update for carousel stories
    
    * Add transition effect to carousel

--- packages/react-compass/src/carousel-v2/content-slider-dots.tsx
+++ packages/react-compass/src/carousel-v2/content-slider-dots.tsx
@@ -1,33 +1,47 @@
 import React from 'react'
+import {StyledComponentProps} from '../utils/stitches.types'
+import {useDOMRef} from '../utils/use-dom-ref'
 import {
-  StyledContentSliderDot,
-  StyledContentSliderDots,
+  StyledCarouselSliderDot,
+  StyledCarouselSliderDots,
 } from './content-slider.styles'
 
-interface Props {
+interface Props extends StyledComponentProps {
   length: number
   current: number
   dotClick?: (index: number) => void
 }
 
-const ContentSliderDots = ({length, current, dotClick = () => {}}: Props) => {
-  return (
-    <StyledContentSliderDots className='content-slider-dots'>
-      {Array(length)
-        .fill(0)
-        .map((item: any, index: number) => {
-          return (
-            <StyledContentSliderDot
-              key={index + item}
-              className={`${index === current ? ' active' : ''}`}
-              onClick={() => {
-                dotClick(index)
-              }}
-            />
-          )
-        })}
-    </StyledContentSliderDots>
-  )
-}
+export type CarouselDotProps = Props &
+  Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
 
-export default ContentSliderDots
+const CarouselSliderDots = React.forwardRef<HTMLDivElement, CarouselDotProps>(
+  (props, ref) => {
+    const {length, current, css = {}, dotClick = () => {}, ...delegated} = props
+    const dotRef = useDOMRef<HTMLDivElement>(ref)
+    return (
+      <StyledCarouselSliderDots
+        ref={dotRef}
+        css={css}
+        {...delegated}
+        className='content-slider-dots'
+      >
+        {Array(length)
+          .fill(0)
+          .map((item: any, index: number) => {
+            return (
+              <StyledCarouselSliderDot
+                key={index + item}
+                className={`${index === current ? ' active' : ''}`}
+                onClick={() => {
+                  dotClick(index)
+                }}
+              />
+            )
+          })}
+      </StyledCarouselSliderDots>
+    )
+  },
+)
+
+export default CarouselSliderDots
