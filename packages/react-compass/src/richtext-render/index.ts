8951c5bbcb25acd2a112eeba80c15bd8d0b95d2d
Tai Bui <75408677+Tai-Chi-Bui@users.noreply.github.com>
2023-02-08 05:03:52 +0700

    Feature: rich text render (#334)

---
+++ packages/react-compass/src/richtext-render/index.ts
@@ -0,0 +1,9 @@
+import {Document} from '@contentful/rich-text-types'
+import RichTextRender from './richtextRender'
+
+export type {RichTextRenderProps} from './richtextRender'
+export type {Document}
+
+RichTextRender.displayName = 'RichTextRender'
+
+export default RichTextRender
---
+++ packages/react-compass/src/richtext-render/richtextRender.tsx
@@ -0,0 +1,43 @@
+import {Document} from '@contentful/rich-text-types'
+import React from 'react'
+import {StyledComponentProps} from '../utils/stitches.types'
+import {useDOMRef} from '../utils/use-dom-ref'
+import RenderDocument from './renderDocument'
+import {
+  StyledRichTextRenderContainer,
+  StyledRichTextRenderContainerProps,
+} from './richtextRender.styles'
+
+interface Props extends StyledComponentProps {
+  document?: Document
+}
+
+export type RichTextRenderProps = Props &
+  StyledRichTextRenderContainerProps &
+  Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
+
+const RichTextRender = React.forwardRef<HTMLInputElement, RichTextRenderProps>(
+  (props, ref) => {
+    const {
+      // StyledComponentProps
+      css = {},
+      // Component
+      document,
+      // element
+      ...delegates
+    } = props
+
+    const richTextRenderRef = useDOMRef<HTMLDivElement>(ref)
+
+    return (
+      <StyledRichTextRenderContainer
+        css={css}
+        ref={richTextRenderRef}
+        {...delegates}
+      >
+        {document ? RenderDocument(document) : null}
+      </StyledRichTextRenderContainer>
+    )
+  },
+)
+export default RichTextRender
