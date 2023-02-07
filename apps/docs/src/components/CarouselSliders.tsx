6d695b2a8a8b7fd0c63d791f663a70d729e3e11e
Tai Bui <tai.c.bui@gmail.com>
2023-02-08 05:56:17 +0700

    Resolve conflict with main

--- apps/docs/src/components/CarouselSliders.tsx
+++ apps/docs/src/components/CarouselSliders.tsx
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:apps/docs/src/components/CarouselSliders.tsx
 import {
   CarouselSlider,
   CarouselSliderButton,
@@ -14,38 +13,16 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import Image from 'next/image'
 import {useState} from 'react'
-========
-// import {
-//   CarouselImageSlide,
-//   CarouselMobile,
-//   CarouselSlide,
-//   CarouselV2,
-//   ContentSlider,
-//   ContentSliderButton,
-//   ContentSliderItem,
-//   Dropdown,
-//   NinePartAlignment,
-//   SocicalIcon,
-// } from '@comfortdelgro/react-compass'
-// import {
-//   faPinterest,
-//   faTelegram,
-//   faTiktok,
-// } from '@fortawesome/free-brands-svg-icons'
-// import Image from 'next/image'
-// import {useState} from 'react'
->>>>>>>> ed5ce42766bd9961ac1a6525c7961b7d6b9e2fba:apps/docs/src/components/ContentSliders.tsx
 
-// const socials: SocicalIcon[] = [
-//   {
-//     icon: faPinterest,
-//     url: 'https://www.pinterest.com.au/',
-//   },
-//   {icon: faTiktok, url: 'https://www.tiktok.com/'},
-//   {icon: faTelegram, url: 'https://telegram.org/'},
-// ]
+const socials: SocicalIcon[] = [
+  {
+    icon: faPinterest,
+    url: 'https://www.pinterest.com.au/',
+  },
+  {icon: faTiktok, url: 'https://www.tiktok.com/'},
+  {icon: faTelegram, url: 'https://telegram.org/'},
+]
 
-<<<<<<<< HEAD:apps/docs/src/components/CarouselSliders.tsx
 const slideData: CarouselSliderItem[] = [
   {
     image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
@@ -167,136 +144,20 @@ export const CarouselMobileFloatingContent = () => {
     </div>
   )
 }
-========
-// const slideData: ContentSliderItem[] = [
-//   {
-//     image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
-//     title: 'Content slider is awesome',
-//     description: 'You can put whatever you want here',
-//     buttons: [
-//       {type: 'primary', label: 'Awesome'},
-//       {type: 'secondary', label: 'Take a tour'},
-//     ],
-//     mask: 0.2,
-//   },
-//   {
-//     image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
-//     title: 'This slide contains buttons',
-//     description: 'These button can listen and handle user click event',
-//     alignment: 'center-end',
-//     buttons: [
-//       {type: 'primary', label: 'Awesome'},
-//       {type: 'secondary', label: 'Take a tour'},
-//     ],
-//     mask: 0.5,
-//   },
-//   {
-//     image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp',
-//     title: 'This slide has different alignment',
-//     description: 'You can use the document for the other alignment options',
-//     alignment: 'end-center',
-//   },
-//   {
-//     image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
-//     title: 'Content slider is awesome',
-//     description: 'You can put whatever you want here',
-//     mask: 0.1,
-//   },
-//   {
-//     image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
-//     title: 'This slide contains buttons',
-//     description: 'These button can listen and handle user click event',
-//     alignment: 'center-end',
-//   },
-//   {
-//     image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp',
-//     title: 'This slide has different alignment',
-//     description: 'You can use the document for the other alignment options',
-//     alignment: 'end-center',
-//     mask: 0.3,
-//   },
-// ]
 
-// const buttons: ContentSliderButton[] = [
-//   {
-//     type: 'secondary',
-//     label: 'Button',
-//     callback: () => {
-//       alert('Button clicked')
-//     },
-//   },
-//   {
-//     type: 'primary',
-//     label: 'Call action',
-//     callback: () => {
-//       alert('Call action clicked')
-//     },
-//   },
-// ]
+export const ImageOnlyDemo = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
 
-// export const PromotionSlider = () => {
-//   return (
-//     <div className='content-slider-sample'>
-//       <CarouselV2 data={slideData} />
-//     </div>
-//   )
-// }
+  const imageUrls = [
+    'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp',
+  ]
 
-// export const AutoPlaySlider = () => {
-//   return (
-//     <div className='content-slider-sample'>
-//       <CarouselV2 data={slideData} autoSwitch={false} />
-//     </div>
-//   )
-// }
+  const handleSwitchSlide = (index: number) => {
+    setActiveIndex(index)
+  }
 
-// export const PromotionSliderWithSocials = () => {
-//   return (
-//     <div className='content-slider-sample'>
-//       <CarouselV2 data={slideData} socials={socials} />
-//     </div>
-//   )
-// }
-
-// export const CarouselMobileMode = () => {
-//   return (
-//     <div style={{width: '320px'}}>
-//       <CarouselMobile
-//         data={slideData}
-//         useNavigation={false}
-//         buttons={buttons}
-//       />
-//     </div>
-//   )
-// }
-
-// export const CarouselMobileFloatingContent = () => {
-//   return (
-//     <div style={{width: '480px'}}>
-//       <CarouselMobile
-//         data={slideData}
-//         floatingContent={true}
-//         buttons={buttons}
-//       />
-//     </div>
-//   )
-// }
->>>>>>>> ed5ce42766bd9961ac1a6525c7961b7d6b9e2fba:apps/docs/src/components/ContentSliders.tsx
-
-// export const ImageOnlyDemo = () => {
-//   const [activeIndex, setActiveIndex] = useState(0)
-
-//   const imageUrls = [
-//     'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
-//     'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp',
-//     'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp',
-//   ]
-
-//   const handleSwitchSlide = (index: number) => {
-//     setActiveIndex(index)
-//   }
-
-<<<<<<<< HEAD:apps/docs/src/components/CarouselSliders.tsx
   return (
     <div className='content-slider-sample'>
       <CarouselSlider onSwitchSlide={handleSwitchSlide}>
@@ -311,37 +172,20 @@ export const CarouselMobileFloatingContent = () => {
     </div>
   )
 }
-========
-//   return (
-//     <div className='content-slider-sample'>
-//       <ContentSlider onSwitchSlide={handleSwitchSlide}>
-//         {imageUrls.map((imageUrl: string, index: number) => (
-//           <CarouselImageSlide
-//             key={index}
-//             active={activeIndex === index}
-//             imageUrl={imageUrl}
-//           />
-//         ))}
-//       </ContentSlider>
-//     </div>
-//   )
-// }
->>>>>>>> ed5ce42766bd9961ac1a6525c7961b7d6b9e2fba:apps/docs/src/components/ContentSliders.tsx
 
-// export const TextNavigationButton = () => {
-//   const [activeIndex, setActiveIndex] = useState(0)
+export const TextNavigationButton = () => {
+  const [activeIndex, setActiveIndex] = useState(0)
 
-//   const imageUrls = [
-//     'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
-//     'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp',
-//     'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp',
-//   ]
+  const imageUrls = [
+    'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp',
+    'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp',
+  ]
 
-//   const handleSwitchSlide = (index: number) => {
-//     setActiveIndex(index)
-//   }
+  const handleSwitchSlide = (index: number) => {
+    setActiveIndex(index)
+  }
 
-<<<<<<<< HEAD:apps/docs/src/components/CarouselSliders.tsx
   return (
     <div className='content-slider-sample'>
       <CarouselSlider
@@ -375,47 +219,11 @@ export const SlideAlignment = () => {
       mask: 0.5,
     },
   ]
-========
-//   return (
-//     <div className='content-slider-sample'>
-//       <ContentSlider
-//         onSwitchSlide={handleSwitchSlide}
-//         navigationButtonType='text'
-//       >
-//         {imageUrls.map((imageUrl: string, index: number) => (
-//           <CarouselImageSlide
-//             key={index}
-//             active={activeIndex === index}
-//             imageUrl={imageUrl}
-//           />
-//         ))}
-//       </ContentSlider>
-//     </div>
-//   )
-// }
 
-// export const SlideAlignment = () => {
-//   const [alignment, setAlignment] = useState<NinePartAlignment>('start-start')
-//   const alignmentData: ContentSliderItem[] = [
-//     {
-//       image: 'https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp',
-//       title: 'This slide contains buttons',
-//       description: 'These button can listen and handle user click event',
-//       alignment,
-//       buttons: [
-//         {type: 'primary', label: 'Awesome'},
-//         {type: 'secondary', label: 'Take a tour'},
-//       ],
-//       mask: 0.5,
-//     },
-//   ]
->>>>>>>> ed5ce42766bd9961ac1a6525c7961b7d6b9e2fba:apps/docs/src/components/ContentSliders.tsx
+  const handleSelectAlignment = (key: any) => {
+    setAlignment(key as NinePartAlignment)
+  }
 
-//   const handleSelectAlignment = (key: any) => {
-//     setAlignment(key as NinePartAlignment)
-//   }
-
-<<<<<<<< HEAD:apps/docs/src/components/CarouselSliders.tsx
   return (
     <>
       <div>
@@ -446,44 +254,11 @@ export const SlideAlignment = () => {
 
 export const SampleCarouselSlider = () => {
   const [activeIndex, setActiveIndex] = useState(0)
-========
-//   return (
-//     <>
-//       <div>
-//         <div style={{width: '300px', padding: '20px'}}>
-//           <Dropdown.Select
-//             label='Select alignment'
-//             placeholder='Select alignment'
-//             onSelectionChange={handleSelectAlignment}
-//           >
-//             <Dropdown.Item key='start-start'>start-start</Dropdown.Item>
-//             <Dropdown.Item key='ccenter-startat'>center-start</Dropdown.Item>
-//             <Dropdown.Item key='end-start'>end-start</Dropdown.Item>
-//             <Dropdown.Item key='start-center'>start-center</Dropdown.Item>
-//             <Dropdown.Item key='center-center'>center-center</Dropdown.Item>
-//             <Dropdown.Item key='end-center'>end-center</Dropdown.Item>
-//             <Dropdown.Item key='start-end'>start-end</Dropdown.Item>
-//             <Dropdown.Item key='center-end'>center-end</Dropdown.Item>
-//             <Dropdown.Item key='end-end'>end-end</Dropdown.Item>
-//           </Dropdown.Select>
-//         </div>
-//         <div className='content-slider-sample'>
-//           <CarouselV2 data={alignmentData} autoSwitch={false} />
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
 
-// export const SampleContentSlider = () => {
-//   const [activeIndex, setActiveIndex] = useState(0)
->>>>>>>> ed5ce42766bd9961ac1a6525c7961b7d6b9e2fba:apps/docs/src/components/ContentSliders.tsx
+  const handleSwitchSlide = (index: number) => {
+    setActiveIndex(index)
+  }
 
-//   const handleSwitchSlide = (index: number) => {
-//     setActiveIndex(index)
-//   }
-
-<<<<<<<< HEAD:apps/docs/src/components/CarouselSliders.tsx
   return (
     <CarouselSlider onSwitchSlide={handleSwitchSlide}>
       <CarouselSlider.Slide active={activeIndex === 0}>
@@ -516,39 +291,3 @@ export const SampleCarouselSlider = () => {
     </CarouselSlider>
   )
 }
-========
-//   return (
-//     <ContentSlider onSwitchSlide={handleSwitchSlide}>
-//       <CarouselSlide active={activeIndex === 0}>
-//         <h3>First slide</h3>
-//         <br />
-//         You can put any content here
-//         <br />
-//         Replace this content with everything you want
-//         <br />
-//         With your own custom styles
-//         <br />
-//         <i>The gray background here is just for this sample</i>
-//       </CarouselSlide>
-//       <CarouselSlide active={activeIndex === 1}>
-//         <h3>Second slide</h3>
-//         <br />
-//         Even if you&apos;re using a <b>rich text content</b>,{' '}
-//         <i>It also works!</i>
-//         <br />
-//         <Image
-//           width={100}
-//           height={100}
-//           alt='sample image'
-//           src='https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp'
-//         />
-//       </CarouselSlide>
-//       <CarouselSlide active={activeIndex === 2}>
-//         <h3>This is the last slide</h3>
-//       </CarouselSlide>
-//     </ContentSlider>
-//   )
-// }
-
-export {}
->>>>>>>> ed5ce42766bd9961ac1a6525c7961b7d6b9e2fba:apps/docs/src/components/ContentSliders.tsx
