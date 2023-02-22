c6ee5593d0bbc8b702531476fbd89aea2a80b740
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-02-22 14:57:28 +0700

    fix

--- packages/react-compass/src/dropdown/utils.tsx
+++ packages/react-compass/src/dropdown/utils.tsx
@@ -242,7 +242,7 @@ import {AriaButtonProps, useButton} from '@react-aria/button'
 // import FlagVEN from '@comfortdelgro/compass-icons/react/flag-ven'
 // import FlagVGB from '@comfortdelgro/compass-icons/react/flag-vgb'
 // import FlagVIR from '@comfortdelgro/compass-icons/react/flag-vir'
-import FlagVNM from '@comfortdelgro/compass-icons/react/flag-vnm'
+// import FlagVNM from '@comfortdelgro/compass-icons/react/flag-vnm'
 // import FlagVUT from '@comfortdelgro/compass-icons/react/flag-vut'
 // import FlagWLF from '@comfortdelgro/compass-icons/react/flag-wlf'
 // import FlagYEM from '@comfortdelgro/compass-icons/react/flag-yem'
@@ -2484,7 +2484,14 @@ export const Flag = ({iso}: {iso: string}) => {
     // case 'VEN':
     //   return <FlagVEN />
     case 'VNM':
-      return <FlagVNM />
+      return (
+        <svg width='16' height='16' viewBox='0 0 16 16' fill='none'>
+          <path
+            d='M8.33276 12.3334C8.02004 12.3334 7.70717 12.2125 7.46885 11.9707L1.35805 5.78022C0.880649 5.29658 0.880649 4.5131 1.35805 4.02947C1.83546 3.54584 2.60886 3.54584 3.08626 4.02947L8.33276 9.34651L13.5804 4.03044C14.0578 3.54681 14.8312 3.54681 15.3086 4.03044C15.786 4.51407 15.786 5.29755 15.3086 5.78118L9.19782 11.9717C8.95912 12.2135 8.64594 12.3334 8.33276 12.3334Z'
+            fill='#201F1E'
+          />
+        </svg>
+      )
     // case 'VGB':
     //   return <FlagVGB />
     // case 'VIR':
