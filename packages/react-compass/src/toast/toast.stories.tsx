f00a9cd932056c63f615d646bcc183b032cd19cc
Tai Bui <tai.c.bui@gmail.com>
2023-02-27 15:54:16 +0700

    hotfix/toast.stories

--- packages/react-compass/src/toast/toast.stories.tsx
+++ packages/react-compass/src/toast/toast.stories.tsx
@@ -1,229 +1,173 @@
-import {styled} from '../theme'
-import {VariantProps} from '../utils/stitches.types'
+import {faImage, faXmark} from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
+import Button from '../button/button'
+import {Icon} from '../icon'
+import {Column} from '../utils/components'
+import Toast from './index'
 
-export const StyledToastActionsContainer = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
+export const Default: React.FC = () => {
+  const [isDefaultOpen, setIsDefaultOpen] = React.useState(false)
 
-  // styling
-  width: '$100%',
-  minHeight: '$6_5',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'flex-start',
-  gap: '$3_25',
-})
+  return (
+    <Column>
+      <h3>Default toast</h3>
+      <Button css={{width: '8rem'}} onPress={() => setIsDefaultOpen(true)}>
+        Open toast
+      </Button>
+      <Toast isOpen={isDefaultOpen} handleClose={() => setIsDefaultOpen(false)}>
+        <Toast.Icon>
+          <Icon icon={faImage} />
+        </Toast.Icon>
+        <Toast.Title>My Title</Toast.Title>
+        <Toast.Label>a few minutes ago</Toast.Label>
+        <Toast.CloseIcon>
+          <Icon icon={faXmark} />
+        </Toast.CloseIcon>
+        <Toast.Message>Lorem ipsum dolor, sit amet consectetur.</Toast.Message>
+        <Toast.Actions>
+          <Button>Take action</Button>
+          <Button>Take action</Button>
+        </Toast.Actions>
+      </Toast>
+    </Column>
+  )
+}
 
-export const StyledToastMessage = styled('p', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
+export const Colors: React.FC = () => {
+  const [isInformativeOpen, setIsInformativeOpen] = React.useState(false)
+  const [isNeutralOpen, setIsNeutralOpen] = React.useState(false)
+  const [isPositiveOpen, setIsPositiveOpen] = React.useState(false)
+  const [isNegativeOpen, setIsNegativeOpen] = React.useState(false)
 
-  // styling
-  width: '$100%',
-  minHeight: '$5_5',
-  fontWeight: '$normal',
-  fontFamily: '$sans',
-  fontSize: '$body3',
-  lineHeight: '$normal',
-})
+  return (
+    <Column>
+      <h3>Informative: blue</h3>
+      <Button css={{width: '8rem'}} onPress={() => setIsInformativeOpen(true)}>
+        Open toast
+      </Button>
+      <Toast
+        isOpen={isInformativeOpen}
+        handleClose={() => setIsInformativeOpen(false)}
+        color='informative'
+      >
+        <Toast.Icon>
+          <Icon icon={faImage} />
+        </Toast.Icon>
+        <Toast.Title>My Title</Toast.Title>
+        <Toast.Label>a few minutes ago</Toast.Label>
+        <Toast.CloseIcon>
+          <Icon icon={faXmark} />
+        </Toast.CloseIcon>
+        <Toast.Message>Lorem ipsum dolor, sit amet consectetur.</Toast.Message>
+        <Toast.Actions>
+          <Button>Take action</Button>
+          <Button>Take action</Button>
+        </Toast.Actions>
+      </Toast>
 
-export const StyledToastCloseIcon = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
+      <h3>Neutral: white</h3>
+      <Button css={{width: '8rem'}} onPress={() => setIsNeutralOpen(true)}>
+        Open toast
+      </Button>
+      <Toast
+        isOpen={isNeutralOpen}
+        handleClose={() => setIsNeutralOpen(false)}
+        color='neutral'
+      >
+        <Toast.Icon>
+          <Icon icon={faImage} />
+        </Toast.Icon>
+        <Toast.Title>My Title</Toast.Title>
+        <Toast.Label>a few minutes ago</Toast.Label>
+        <Toast.CloseIcon>
+          <Icon icon={faXmark} />
+        </Toast.CloseIcon>
+        <Toast.Message>Lorem ipsum dolor, sit amet consectetur.</Toast.Message>
+        <Toast.Actions>
+          <Button>Take action</Button>
+          <Button>Take action</Button>
+        </Toast.Actions>
+      </Toast>
 
-  // styling
-  minWidth: '$3_5',
-  maxWidth: '$3_5',
-  minHeight: '$3_5',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  cursor: 'pointer',
+      <h3>Negative: red</h3>
+      <Button css={{width: '8rem'}} onPress={() => setIsNegativeOpen(true)}>
+        Open toast
+      </Button>
+      <Toast
+        isOpen={isNegativeOpen}
+        handleClose={() => setIsNegativeOpen(false)}
+        color='negative'
+      >
+        <Toast.Icon>
+          <Icon icon={faImage} />
+        </Toast.Icon>
+        <Toast.Title>My Title</Toast.Title>
+        <Toast.Label>a few minutes ago</Toast.Label>
+        <Toast.CloseIcon>
+          <Icon icon={faXmark} />
+        </Toast.CloseIcon>
+        <Toast.Message>Lorem ipsum dolor, sit amet consectetur.</Toast.Message>
+        <Toast.Actions>
+          <Button>Take action</Button>
+          <Button>Take action</Button>
+        </Toast.Actions>
+      </Toast>
 
-  '& *': {
-    width: '100%',
-    height: '100%',
-  },
-})
+      <h3>Positive: green</h3>
+      <Button css={{width: '8rem'}} onPress={() => setIsPositiveOpen(true)}>
+        Open toast
+      </Button>
+      <Toast
+        isOpen={isPositiveOpen}
+        handleClose={() => setIsPositiveOpen(false)}
+        color='positive'
+      >
+        <Toast.Icon>
+          <Icon icon={faImage} />
+        </Toast.Icon>
+        <Toast.Title>My Title</Toast.Title>
+        <Toast.Label>a few minutes ago</Toast.Label>
+        <Toast.CloseIcon>
+          <Icon icon={faXmark} />
+        </Toast.CloseIcon>
+        <Toast.Message>Lorem ipsum dolor, sit amet consectetur.</Toast.Message>
+        <Toast.Actions>
+          <Button>Take action</Button>
+          <Button>Take action</Button>
+        </Toast.Actions>
+      </Toast>
+    </Column>
+  )
+}
 
-export const StyledToastLabel = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
-
-  // styling
-  width: 'fit-content',
-  minHeight: '$4_5',
-  fontWeight: '$normal',
-  fontFamily: '$sans',
-  fontSize: '$label2',
-  color: '$gray70',
-  lineHeight: '$normal',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-})
-
-export const StyledToastTitle = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
-
-  // styling
-  width: 'fit-content',
-  minHeight: '$4_5',
-  fontWeight: '$semibold',
-  fontFamily: '$sans',
-  fontSize: '$label1',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-})
-
-export const StyledToastIcon = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
-
-  // styling
-  minWidth: '$5_5',
-  maxWidth: '$5_5',
-  minHeight: '$4_5',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-
-  '& *': {
-    width: '100%',
-    height: '100%',
-  },
-})
-
-export const StyledToastHeaderLeft = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
-
-  // styling
-  minHeight: '$5_25',
-  display: 'flex',
-  justifyContent: 'flex-start',
-  gap: '$4_25',
-  alignItems: 'center',
-})
-
-export const StyledToastHeaderRight = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
-
-  // styling
-  minHeight: '$5_25',
-  display: 'flex',
-  justifyContent: 'flex-end',
-  gap: '$4_25',
-  alignItems: 'center',
-})
-
-export const StyledToastHeader = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  background: 'transparent',
-  margin: '0px',
-  padding: '0px',
-
-  // header styling
-  width: '100%',
-  minHeight: '$5_25',
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-})
-
-export const StyledToast = styled('div', {
-  // reset
-  appearance: 'none',
-  border: 'none',
-  boxSizing: 'border-box',
-  //background: 'transparent',
-  margin: '0px',
-  //padding: '0px',
-
-  // toast container styling
-  width: '$110_5',
-  //minHeight: '$24_5',
-  borderRadius: '$md',
-  boxShadow:
-    '0px 0.6px 1.8px rgba(0, 0, 0, 0.1), 0px 3.2px 7.2px rgba(0, 0, 0, 0.13)',
-  backgroundColor: '$background',
-  padding: '$4',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '$4',
-  zIndex: '60',
-
-  //center
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  /* bring your own prefixes */
-  transform: `translate(-50%, -50%)`,
-
-  variants: {
-    color: {
-      informative: {
-        backgroundColor: '$cdgBlue20',
-      }, //blue
-      neutral: {},
-      negative: {
-        backgroundColor: '$dangerBg',
-      }, //red
-      positive: {
-        backgroundColor: '$successBg',
-      }, //green
-    },
-  },
-
-  defaultVariants: {
-    color: 'neutral',
-  },
-})
-
-export type ToastVariantProps = VariantProps<typeof StyledToast>
+export const AutoClose: React.FC = () => {
+  const [isAutoCloseOpen, setIsAutoCloseOpen] = React.useState(false)
+  return (
+    <Column>
+      <h3>Auto-close toast</h3>
+      <Button css={{width: '8rem'}} onPress={() => setIsAutoCloseOpen(true)}>
+        Open toast
+      </Button>
+      <Toast
+        isOpen={isAutoCloseOpen}
+        handleClose={() => setIsAutoCloseOpen(false)}
+        autoClose={500}
+        color='informative'
+      >
+        <Toast.Icon>
+          <Icon icon={faImage} />
+        </Toast.Icon>
+        <Toast.Title>My Title</Toast.Title>
+        <Toast.Label>a few minutes ago</Toast.Label>
+        <Toast.CloseIcon>
+          <Icon icon={faXmark} />
+        </Toast.CloseIcon>
+        <Toast.Message>Lorem ipsum dolor, sit amet consectetur.</Toast.Message>
+        <Toast.Actions>
+          <Button>Take action</Button>
+          <Button>Take action</Button>
+        </Toast.Actions>
+      </Toast>
+    </Column>
+  )
+}
