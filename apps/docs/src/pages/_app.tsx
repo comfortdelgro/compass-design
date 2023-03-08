24d1798baaaed3f75bd42622865c4680791ba57b
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-03-08 22:27:42 +0700

    [yagin][fix warning stage 1]

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Preflight from '@comfortdelgro/react-compass/preflight'
+import { SSRProvider } from '@react-aria/ssr'
 import {AppProps} from 'next/app'
 import {Toaster} from 'react-hot-toast'
 import 'styles/global.css'
@@ -6,11 +7,11 @@ import 'styles/tailwind.css'
 
 const App: React.FC<AppProps> = ({Component, pageProps}) => {
   return (
-    <>
+    <SSRProvider>
       <Preflight />
       <Toaster position='bottom-right' />
       <Component {...pageProps} />
-    </>
+    </SSRProvider>
   )
 }
 
