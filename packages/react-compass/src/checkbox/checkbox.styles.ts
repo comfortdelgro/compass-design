168c12b98c1c0a14ad1256c94c79d37e4335a179
Tai Bui <tai.c.bui@gmail.com>
2023-03-09 10:20:14 +0700

    hotfix/dublicate

--- packages/react-compass/src/checkbox/checkbox.styles.ts
+++ packages/react-compass/src/checkbox/checkbox.styles.ts
@@ -50,59 +50,6 @@ export const StyledCheckboxBox = styled('span', {
   },
 })
 
-export const StyledTableCheckboxBox = styled('span', {
-  display: 'block',
-  height: '$4',
-  width: '$4',
-  backgroundColor: '$whiteText',
-  borderWidth: '$light',
-  borderStyle: 'solid',
-  borderColor: '$activeBorder',
-  borderRadius: '$md',
-  transition: '$button',
-
-  variants: {
-    rounded: {
-      true: {
-        borderRadius: '$full',
-      },
-    },
-
-    disabled: {
-      true: {
-        backgroundColor: '$whiteText',
-        borderColor: '$gray50',
-      },
-    },
-  },
-})
-export const StyledTableCheckboxBox = styled('span', {
-  display: 'block',
-  height: '$4',
-  width: '$4',
-  backgroundColor: '$whiteText',
-  borderWidth: '$light',
-  borderStyle: 'solid',
-  borderColor: '$activeBorder',
-  borderRadius: '$md',
-  transition: '$button',
-
-  variants: {
-    rounded: {
-      true: {
-        borderRadius: '$full',
-      },
-    },
-
-    disabled: {
-      true: {
-        backgroundColor: '$whiteText',
-        borderColor: '$gray50',
-      },
-    },
-  },
-})
-
 export const StyledCheckboxCheckmark = styled('span', {
   display: 'none',
   color: '$whiteText',
