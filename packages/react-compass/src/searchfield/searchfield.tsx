9af6caaa6678b5ab725600d5f8c9dc8ddd4d4562
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-04-04 10:01:10 +0700

    feat: remove react aria out of searchfield

--- packages/react-compass/src/searchfield/searchfield.tsx
+++ packages/react-compass/src/searchfield/searchfield.tsx
@@ -1,7 +1,4 @@
 import {faClose, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
-import {useSearchField} from '@react-aria/searchfield'
-import {useSearchFieldState} from '@react-stately/searchfield'
-import type {AriaSearchFieldProps} from '@react-types/searchfield'
 import React from 'react'
 import Button from '../button/button'
 import {Icon} from '../icon'
@@ -13,8 +10,54 @@ import {
   StyledSearchFieldInput,
 } from './searchfield.styles'
 
-interface Props extends AriaSearchFieldProps, StyledComponentProps {
+interface Props extends StyledComponentProps {
+  id?: string
   isErrored?: boolean
+  isDisabled?: boolean
+  isReadOnly?: boolean
+  validationState?: 'valid' | 'invalid'
+  isRequired?: boolean
+  description?: React.ReactNode
+  errorMessage?: React.ReactNode
+  autoFocus?: boolean
+  placeholder?: string
+  value?: string
+  defaultValue?: string
+  label?: React.ReactNode
+  onSubmit?: (value: string) => void
+  onClear?: () => void
+  onChange?: (value: string) => void
+  onChangeEvent?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onCopy?: React.ClipboardEventHandler<HTMLInputElement>
+  onCut?: React.ClipboardEventHandler<HTMLInputElement>
+  onPaste?: React.ClipboardEventHandler<HTMLInputElement>
+  onCompositionStart?: React.CompositionEventHandler<HTMLInputElement>
+  onCompositionEnd?: React.CompositionEventHandler<HTMLInputElement>
+  onCompositionUpdate?: React.CompositionEventHandler<HTMLInputElement>
+  onSelect?: React.ReactEventHandler<HTMLInputElement>
+  onBeforeInput?: React.FormEventHandler<HTMLInputElement>
+  onInput?: React.FormEventHandler<HTMLInputElement>
+  onFocus?: (e: React.FocusEvent) => void
+  onBlur?: (e: React.FocusEvent) => void
+  onKeyDown?: (e: React.KeyboardEvent) => void
+  onKeyUp?: (e: React.KeyboardEvent) => void
+  'aria-activedescendant'?: string
+  'aria-autocomplete'?: 'none' | 'inline' | 'list' | 'both'
+  'aria-haspopup'?:
+    | boolean
+    | 'false'
+    | 'true'
+    | 'menu'
+    | 'listbox'
+    | 'tree'
+    | 'grid'
+    | 'dialog'
+  'aria-controls'?: string
+  'aria-label'?: string
+  'aria-labelledby'?: string
+  'aria-describedby'?: string
+  'aria-details'?: string
+  'aria-errormessage'?: string
 }
 
 export type SearchFieldProps = Props & SearchFieldVariantProps
@@ -24,45 +67,111 @@ const SearchField = React.forwardRef<HTMLInputElement, SearchFieldProps>(
     const {
       // StyledComponentProps
       css = {},
+      id,
+      value = '',
       isErrored = false,
-      // AriaSearchFieldProps
-      isDisabled,
-      ...ariaSafeProps
+      isReadOnly = false,
+      isRequired = false,
+      isDisabled = false,
+      autoFocus,
+      onChangeEvent,
+      onChange,
     } = props
 
-    const ariaProps = {
-      isDisabled,
-      label: 'Search',
-      ...ariaSafeProps,
-    } as AriaSearchFieldProps
-
-    const state = useSearchFieldState(ariaProps)
+    const [textValue, setTextValue] = React.useState<string>(value)
     const searchFieldRef = useDOMRef<HTMLInputElement>(ref)
-    const {inputProps, clearButtonProps} = useSearchField(
-      ariaProps,
-      state,
-      searchFieldRef,
-    )
+
+    React.useEffect(() => {
+      setTextValue(value)
+    }, [value])
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const v = event.target.value
+      setTextValue(v)
+      onChange?.(v)
+      onChangeEvent?.(event)
+    }
+
+    const handleOnKeyDown = (e: React.KeyboardEvent) => {
+      const key = e.key
+      const target = e.target as HTMLInputElement
+
+      if (key === 'Enter' || key === 'Escape') {
+        e.preventDefault()
+      }
+
+      if (isDisabled || isReadOnly) {
+        return
+      }
+
+      if (key === 'Enter' && target.value != '') {
+        props.onSubmit?.(target.value)
+      }
+
+      if (key === 'Escape') {
+        setTextValue('')
+        props.onClear?.()
+      }
+      props.onKeyDown?.(e)
+    }
+
+    const onClearButtonClick = () => {
+      setTextValue('')
+      onChange?.('')
+      props.onClear?.()
+    }
+
+    const onSearchButtonClick = () => {
+      searchFieldRef.current?.focus()
+    }
 
     return (
       <StyledSearchFieldBox
-        disabled={!!isDisabled}
+        disabled={isDisabled}
         css={css}
         isErrored={isErrored}
       >
         <StyledSearchFieldInput
           ref={searchFieldRef}
-          {...inputProps}
+          id={id}
+          autoFocus={autoFocus}
+          readOnly={isReadOnly}
+          required={isRequired}
+          disabled={isDisabled}
           type='text'
+          value={textValue}
+          onCut={props.onCut}
+          onCopy={props.onCopy}
+          onBlur={props.onBlur}
+          onFocus={props.onFocus}
+          onPaste={props.onPaste}
+          onInput={props.onInput}
+          onKeyUp={props.onKeyUp}
+          onSelect={props.onSelect}
+          onChange={handleOnChange}
+          onKeyDown={handleOnKeyDown}
+          onBeforeInput={props.onBeforeInput}
+          onCompositionEnd={props.onCompositionEnd}
+          onCompositionStart={props.onCompositionStart}
+          onCompositionUpdate={props.onCompositionUpdate}
+          aria-label={props['aria-label']}
+          aria-details={props['aria-details']}
+          aria-haspopup={props['aria-haspopup']}
+          aria-controls={props['aria-controls']}
+          aria-labelledby={props['aria-labelledby']}
+          aria-describedby={props['aria-describedby']}
+          aria-errormessage={props['aria-errormessage']}
+          aria-autocomplete={props['aria-autocomplete']}
+          aria-activedescendant={props['aria-activedescendant']}
         />
-        {state.value !== '' ? (
-          <Button {...clearButtonProps} size='sm' variant='ghost'>
+        {textValue !== '' ? (
+          <Button size='sm' variant='ghost' onPress={onClearButtonClick}>
             <Icon className='icon' icon={faClose} />
           </Button>
         ) : (
           <Button
             isDisabled={!!isDisabled}
-            onPress={() => searchFieldRef.current?.focus()}
+            onPress={onSearchButtonClick}
             size='sm'
             variant='ghost'
           >
