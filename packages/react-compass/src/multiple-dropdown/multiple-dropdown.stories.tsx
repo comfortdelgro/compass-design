c94d31856a0da3dfa1fdc3a2fb948c98ab63d93f
michael-le-19 <minhlt19@fsoft.com.vn>
2023-05-04 09:44:39 +0700

    fix: remove x icon if multiple dropdown is disabled

--- packages/react-compass/src/multiple-dropdown/multiple-dropdown.stories.tsx
+++ packages/react-compass/src/multiple-dropdown/multiple-dropdown.stories.tsx
@@ -139,21 +139,26 @@ export const Default: React.FC = () => {
         <MultipleDropdown.Item key='snake'>Snake</MultipleDropdown.Item>
       </MultipleDropdown>
 
-      <h3>With Section</h3>
-      <MultipleDropdown label='Favorite Animal' placeholder='Choose an animal'>
-        <MultipleDropdown.Item key='fly'>Fly</MultipleDropdown.Item>
-        <MultipleDropdown.Section title='Group 1'>
-          <MultipleDropdown.Item key='red panda'>
-            Red Panda
-          </MultipleDropdown.Item>
-          <MultipleDropdown.Item key='cat'>Cat</MultipleDropdown.Item>
-          <MultipleDropdown.Item key='dog'>Dog</MultipleDropdown.Item>
-        </MultipleDropdown.Section>
-        <MultipleDropdown.Section title='Group 2'>
-          <MultipleDropdown.Item key='aardvark'>Aardvark</MultipleDropdown.Item>
-          <MultipleDropdown.Item key='kangaroo'>Kangaroo</MultipleDropdown.Item>
-          <MultipleDropdown.Item key='snake'>Snake</MultipleDropdown.Item>
-        </MultipleDropdown.Section>
+      <h3>Disabled</h3>
+      <MultipleDropdown
+        label='Favorite Animal'
+        disabledKeys={['cat']}
+        placeholder='Choose an animal'
+        selectedKeys={value}
+        defaultSelectedKeys={['cat']}
+        isRequired
+        onSelectionChange={(k: Key[]) => setValue(k)}
+        isDisabled
+      >
+        <MultipleDropdown.Item key='red panda'>
+          <Icon icon={faExclamationTriangle} style={{marginRight: 5}} />
+          Red Panda
+        </MultipleDropdown.Item>
+        <MultipleDropdown.Item key='cat'>Cat</MultipleDropdown.Item>
+        <MultipleDropdown.Item key='dog'>Dog</MultipleDropdown.Item>
+        <MultipleDropdown.Item key='aardvark'>Aardvark</MultipleDropdown.Item>
+        <MultipleDropdown.Item key='kangaroo'>Kangaroo</MultipleDropdown.Item>
+        <MultipleDropdown.Item key='snake'>Snake</MultipleDropdown.Item>
       </MultipleDropdown>
     </Column>
   )
