eb6e1236b6e92805c8969e60419924201a9ea183
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-05-15 10:32:28 +0700

    fix: export placement from tooltip and cb props

--- packages/react-compass/src/checkbox/checkbox.tsx
+++ packages/react-compass/src/checkbox/checkbox.tsx
@@ -14,19 +14,40 @@ import {
 } from './checkbox.styles'
 
 interface Props extends StyledComponentProps {
+  id?: string
+  name?: string
   isIndeterminate?: boolean
   children?: React.ReactNode
-  onChange?: (isSelected: boolean) => void
   isDisabled?: boolean
   defaultSelected?: boolean
   isSelected?: boolean
   // Variants for children
   variant?: 'default' | 'rounded'
+  onChange?: (isSelected: boolean) => void
+
+  autoFocus?: boolean
+  'aria-activedescendant'?: string
+  'aria-autocomplete'?: 'none' | 'inline' | 'list' | 'both'
+  'aria-haspopup'?:
+    | boolean
+    | 'false'
+    | 'true'
+    | 'menu'
+    | 'listbox'
+    | 'tree'
+    | 'grid'
+    | 'dialog'
+  'aria-controls'?: string
+  'aria-label'?: string
+  'aria-labelledby'?: string
+  'aria-describedby'?: string
+  'aria-details'?: string
+  'aria-errormessage'?: string
 }
 
 export type CheckboxProps = Props &
   CheckboxVariantProps &
-  Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
+  Omit<React.HTMLAttributes<HTMLInputElement>, keyof Props>
 
 const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
   (props, ref) => {
@@ -80,10 +101,10 @@ const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
           <StyledCheckboxInput
             type='checkbox'
             ref={checkboxRef}
+            {...ariaProps}
             checked={checked}
             disabled={isDisabled}
             onChange={handleCheckboxChange}
-            {...ariaProps}
           />
 
           {/* Checkbox */}
