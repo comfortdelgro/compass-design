bf07f7bd71f801b6920f28333897d5842eb7c6cf
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-05-19 11:40:55 +0700

    [yagin][add input data grid]

--- packages/react-compass/src/data-grid/data-grid-cell.tsx
+++ packages/react-compass/src/data-grid/data-grid-cell.tsx
@@ -1,5 +1,5 @@
 import {Cell, flexRender} from '@tanstack/react-table'
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {StyledComponentProps} from '../utils/stitches.types'
 import {useDOMRef} from '../utils/use-dom-ref'
 import {StyledDataGridCell} from './data-grid-cell.styles'
@@ -12,19 +12,63 @@ export type DataGridCellProps = Props
 
 const DataGridCell = React.forwardRef<HTMLTableCellElement, DataGridCellProps>(
   ({cell}, ref) => {
-    const tableRowRef = useDOMRef<HTMLTableCellElement>(ref)
+    const tableCellRef = useDOMRef<HTMLTableCellElement>(ref)
+    const [editing, setEditing] = useState(false)
+    const [value, setValue] = useState(cell.getContext())
+    const inputRef = useRef<HTMLInputElement>(null)
+    const handleDoubleClick = () => {
+      setEditing(true)
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      // setValue(event.target.value)
+    }
+
+    const handleBlur = () => {
+      setEditing(false)
+      // cell.updateValue(value)
+    }
+
+    useEffect(() => {
+      const handleClickOutside = (event: MouseEvent) => {
+        if (
+          editing &&
+          inputRef.current &&
+          !inputRef.current.contains(event.target as Node)
+        ) {
+          handleBlur()
+        }
+      }
+
+      document.addEventListener('mousedown', handleClickOutside)
+
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
+    }, [editing])
 
     return (
       <StyledDataGridCell
-        ref={tableRowRef}
+        ref={tableCellRef}
         {...{
           key: cell.id,
           style: {
             width: cell.column.getSize(),
           },
+          onDoubleClick: handleDoubleClick,
+          onBlur: handleBlur,
         }}
       >
-        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+        {editing ? (
+          <input
+            ref={inputRef}
+            type='text'
+            // value={value}
+            onChange={handleChange}
+          />
+        ) : (
+          flexRender(cell.column.columnDef.cell, cell.getContext())
+        )}
       </StyledDataGridCell>
     )
   },
