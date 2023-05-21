8bd86f78e82ab4da4a925f0bb22954d898bb3f0e
hoapn-fsoft <128688086+hoapn-fsoft@users.noreply.github.com>
2023-05-22 06:37:03 +0700

    add: license for calendar file (#686)

--- packages/react-compass/src/calendar/hooks/useSpinButton.ts
+++ packages/react-compass/src/calendar/hooks/useSpinButton.ts
@@ -1,3 +1,13 @@
+/**
+ * This file contains code adapted from the React Spectrum project.
+ *
+ * Copyright (c) 2018, Adobe Inc. and other contributors.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Apache License, Version 2.0
+ * found in the LICENSE.txt file at the root directory of this source tree.
+ */
+
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
