588685e334bf1d7d24815c150fc90fc48a430388
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-05-22 23:04:35 +0700

    [yagin][delete Filter ]

--- packages/react-compass/src/table-v2/react-table-column-header.tsx
+++ packages/react-compass/src/table-v2/react-table-column-header.tsx
@@ -1,4 +1,4 @@
-import {Column, flexRender, Header, Table} from '@tanstack/react-table'
+import {flexRender, Header} from '@tanstack/react-table'
 import React from 'react'
 import {useDOMRef} from '../utils/use-dom-ref'
 import {
@@ -101,56 +101,5 @@ const ReactTableColumnHeader = React.forwardRef<HTMLTableCellElement, Props>(
     )
   },
 )
-
-function Filter({
-  column,
-  table,
-}: {
-  column: Column<any, any>
-  table: Table<any>
-}) {
-  const firstValue = table
-    .getPreFilteredRowModel()
-    .flatRows[0]?.getValue(column.id)
-
-  const columnFilterValue = column.getFilterValue()
-
-  return typeof firstValue === 'number' ? (
-    <div className='flex space-x-2'>
-      <input
-        type='number'
-        value={(columnFilterValue as [number, number])?.[0] ?? ''}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [
-            e.target.value,
-            old?.[1],
-          ])
-        }
-        placeholder={`Min`}
-        className='w-24 border shadow rounded'
-      />
-      <input
-        type='number'
-        value={(columnFilterValue as [number, number])?.[1] ?? ''}
-        onChange={(e) =>
-          column.setFilterValue((old: [number, number]) => [
-            old?.[0],
-            e.target.value,
-          ])
-        }
-        placeholder={`Max`}
-        className='w-24 border shadow rounded'
-      />
-    </div>
-  ) : (
-    <input
-      type='text'
-      value={(columnFilterValue ?? '') as string}
-      onChange={(e) => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      className='w-36 border shadow rounded'
-    />
-  )
-}
 
 export default ReactTableColumnHeader
