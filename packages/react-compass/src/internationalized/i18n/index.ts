5447d1acba9102431131d2917a7e4675a847617a
Hai Do Tien <dotienhai1611@gmail.com>
2023-05-28 23:07:40 +0700

    export i18n

--- packages/react-compass/src/internationalized/i18n/index.ts
+++ packages/react-compass/src/internationalized/i18n/index.ts
@@ -1,7 +1,1 @@
-export {I18nProvider, useLocale} from './context'
-export type {I18nProviderProps, Locale} from './context'
-export {useCollator} from './useCollator'
-export {useDateFormatter} from './useDateFormatter'
-export type {DateFormatterOptions} from './useDateFormatter'
-export {useFilter} from './useFilter'
-export type {Filter} from './useFilter'
+export * from './src'
