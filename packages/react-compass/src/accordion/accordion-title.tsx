a615d74d600fce1fd00393772239737191656e7f
TungNT107 <117718974+TungNT107@users.noreply.github.com>
2023-06-05 03:37:55 +0700

    Replace all icon with svg (#764)
    * feat: replace icons
    
    * feat: replace icons

--- packages/react-compass/src/accordion/accordion-title.tsx
+++ packages/react-compass/src/accordion/accordion-title.tsx
@@ -1,14 +1,11 @@
-import {faQuestionCircle} from '@fortawesome/free-regular-svg-icons'
-import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
 import React, {useContext} from 'react'
-import {Icon, IconProp} from '../icon'
 import {StyledComponentProps} from '../utils/stitches.types'
 import AccordionButton from './accordion-button'
 import AccordionContext, {AccordionContextType} from './accordion-context'
 import {StyledAccordionTitleWrapper} from './accordion-title.styles'
 
 interface Props extends StyledComponentProps {
-  icon?: false | IconProp
+  icon?: false | React.ReactNode
   children?: string | React.ReactNode
   expandIcon?: React.ReactNode
 }
@@ -19,7 +16,7 @@ export type AccordionTitleProps = Props &
 const AccordionTitle = React.forwardRef<HTMLButtonElement, AccordionTitleProps>(
   (props: AccordionTitleProps, ref) => {
     const {
-      icon = faQuestionCircle,
+      icon = <DefaultIcon />,
       children,
       expandIcon,
       css = {},
@@ -51,11 +48,7 @@ const AccordionTitle = React.forwardRef<HTMLButtonElement, AccordionTitleProps>(
 
     const renderLeftIcon = () => {
       if (icon === false) return null
-      return (
-        <div className='accordion-left-icon-container'>
-          <Icon icon={icon} className='accordion-left-icon' />
-        </div>
-      )
+      return <div className='accordion-left-icon-container'>{icon}</div>
     }
 
     return (
@@ -76,13 +69,27 @@ const AccordionTitle = React.forwardRef<HTMLButtonElement, AccordionTitleProps>(
             {expandIcon ? (
               expandIcon
             ) : (
-              <Icon className='accordion-chevron-icon' icon={faChevronDown} />
+              <svg viewBox='0 0 512 512'>
+                <path
+                  fill='currentColor'
+                  d='M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z'
+                />
+              </svg>
             )}
           </div>
         </StyledAccordionTitleWrapper>
       </AccordionButton>
     )
   },
+)
+
+const DefaultIcon = () => (
+  <svg viewBox='0 0 512 512'>
+    <path
+      fill='currentColor'
+      d='M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm169.8-90.7c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z'
+    />
+  </svg>
 )
 
 export default AccordionTitle
--- packages/react-compass/src/calendar/calendar-header.tsx
+++ packages/react-compass/src/calendar/calendar-header.tsx
@@ -1,10 +1,8 @@
-import {faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons'
 import {DateValue} from '@internationalized/date'
 import {useDateFormatter} from '@react-aria/i18n'
 import {VisuallyHidden} from '@react-aria/visually-hidden'
 import React from 'react'
 import Button, {ButtonProps} from '../button'
-import Icon from '../icon'
 import {StyledComponentProps} from '../utils/stitches.types'
 import {StyledCalendarHeader} from './calendar-header.style'
 import {AriaLabelingProps, DOMProps} from './types'
@@ -52,10 +50,15 @@ const CalendarHeader = (props: Props) => {
       </VisuallyHidden>
       <div className='calendar-header-left-side'>
         <Button variant='ghost' {...prevButtonProps}>
-          <Icon
+          <svg
             className='calendar-header-icon chevron-left'
-            icon={faChevronLeft}
-          />
+            viewBox='0 0 320 512'
+          >
+            <path
+              fill='currentColor'
+              d='M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z'
+            />
+          </svg>
         </Button>
         <h2
           // We have a visually hidden heading describing the entire visible range,
@@ -69,10 +72,15 @@ const CalendarHeader = (props: Props) => {
         </h2>
         {variant === 'default' && (
           <Button variant='ghost' {...nextButtonProps}>
-            <Icon
+            <svg
               className='calendar-header-icon chevron-right'
-              icon={faChevronRight}
-            />
+              viewBox='0 0 320 512'
+            >
+              <path
+                fill='currentColor'
+                d='M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z'
+              />
+            </svg>
           </Button>
         )}
       </div>
@@ -86,10 +94,15 @@ const CalendarHeader = (props: Props) => {
             )}
           </h2>
           <Button variant='ghost' {...nextButtonProps}>
-            <Icon
+            <svg
               className='calendar-header-icon chevron-right'
-              icon={faChevronRight}
-            />
+              viewBox='0 0 320 512'
+            >
+              <path
+                fill='currentColor'
+                d='M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z'
+              />
+            </svg>
           </Button>
         </div>
       )}
