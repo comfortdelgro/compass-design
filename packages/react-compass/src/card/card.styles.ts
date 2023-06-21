6a26c3c4cbab4e951ea004e6469d8e19a607bb7d
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-06-21 10:02:11 +0700

    fix: CDS-372

--- packages/react-compass/src/card/card.styles.ts
+++ packages/react-compass/src/card/card.styles.ts
@@ -6,7 +6,7 @@ import {StyledCardTitle} from './card-title.styles'
 export const StyledCard = styled('div', {
   width: '100%',
   position: 'relative',
-  backgroundColor: '$background',
+  backgroundColor: '$secondaryBg',
   boxShadow:
     '0px 0.3px 0.9px rgba(0, 0, 0, 0.1), 0px 1.6px 3.6px rgba(0, 0, 0, 0.13)',
   borderRadius: '$md',
