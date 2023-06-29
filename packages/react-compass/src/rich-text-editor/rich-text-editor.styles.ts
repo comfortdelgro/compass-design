db2c5f526ffcfc1dbd2a79520773329ba7d8227f
Hai Do Tien <dotienhai1611@gmail.com>
2023-06-29 10:31:48 +0700

    more samples rich text editor (#821)

--- packages/react-compass/src/rich-text-editor/rich-text-editor.styles.ts
+++ packages/react-compass/src/rich-text-editor/rich-text-editor.styles.ts
@@ -2,8 +2,6 @@ import { styled } from '../theme'
 import type { VariantProps } from '../utils/stitches.types'
 
 
-
-
 export const StyledEditorContent = styled('div', {
   // reset
   appearance: 'none',
@@ -12,6 +10,8 @@ export const StyledEditorContent = styled('div', {
   margin: '0px',
   backgroundColor: '$background',
   color: '$primaryText',
+  height: '$125',
+  overflow: 'scroll',
   // styling
 })
 
@@ -55,6 +55,14 @@ export const StyledRichTextEditor = styled('div', {
     color: '$info',
     textDecoration: 'underline',
     transition: '$default',
+  },
+
+  '& .ProseMirror p.is-editor-empty:first-child::before': {
+    color: '$tertiaryText',
+    content: 'attr(data-placeholder)',
+    float: 'left',
+    height: 0,
+    pointerEvents: 'none',
   },
   h1: {
     display: 'block',
