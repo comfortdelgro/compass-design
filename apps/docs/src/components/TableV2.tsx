510cc9ef1af645e4ce489a1a83937db3f4930f1f
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-06-30 00:23:00 +0700

    [yagin][remove console log]

--- apps/docs/src/components/TableV2.tsx
+++ apps/docs/src/components/TableV2.tsx
@@ -178,10 +178,6 @@ export const ReactTableFullFeature: React.FC = () => {
   }
 
   const TableHeaderCell = ({row}: any) => {
-<<<<<<< HEAD
-=======
-    console.log('=========', row.getCanSelect())
->>>>>>> 846528b55396063c451d67867d9fffa6ac7ae824
     return (
       <div className='px-1'>
         <ReactTable.CheckboxCell
