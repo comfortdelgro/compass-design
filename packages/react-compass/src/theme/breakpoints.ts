a96b5b2c033bcff1d7b5406aa1706f90151b67c1
TungNT107 <117718974+TungNT107@users.noreply.github.com>
2023-07-05 09:21:10 +0700

    fix: CDS-342 (#849)

--- packages/react-compass/src/theme/breakpoints.ts
+++ packages/react-compass/src/theme/breakpoints.ts
@@ -6,6 +6,18 @@ const breakpoints = {
   md: '(min-width: 900px)',
   lg: '(min-width: 1200px)',
   xl: '(min-width: 1536px)',
+
+  min_320: '(min-width: 320px)',
+  min_480: '(min-width: 480px)',
+  min_768: '(min-width: 768px)',
+  min_1024: '(min-width: 1024px)',
+  min_1200: '(min-width: 1200px)',
+
+  max_320: '(max-width: 320px)',
+  max_480: '(max-width: 480px)',
+  max_768: '(max-width: 768px)',
+  max_1024: '(max-width: 1024px)',
+  max_1200: '(max-width: 1200px)',
 }
 
 export default breakpoints
