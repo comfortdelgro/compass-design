ef12280768516997c6d6f6a816e9dba843fa895d
hoapn-fsoft <hoapn3@fsoft.com.vn>
2023-07-08 17:00:58 +0700

    remove other page

--- apps/docs-v2/pages/experiments/docs/headers.md
+++
@@ -1,5 +0,0 @@
-# Headers
-
-<p class="description">Headers.</p>
-
-## Header with Pro plan [<span class="plan-pro"></span>](/x/introduction/licensing/#pro-plan)
--- apps/docs-v2/pages/joy-ui/getting-started/index.js
+++
@@ -1,6 +0,0 @@
-import * as pageProps from 'docs/data/joy/getting-started/overview/overview.md?@comfortdelgro/markdown'
-import MarkdownDocs from 'docs/src/modules/components/MarkdownDocs'
-
-export default function Page() {
-  return <MarkdownDocs {...pageProps} />
-}
