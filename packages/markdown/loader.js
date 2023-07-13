949a5f98c19245fef9f82b9d12b4f7384e0324b8
hoapn-fsoft <hoapn3@fsoft.com.vn>
2023-07-13 14:23:29 +0700

    refactor: loader markdown

--- packages/markdown/loader.js
+++ packages/markdown/loader.js
@@ -5,19 +5,10 @@ const extractImports = require('./extractImports')
 
 const notEnglishMarkdownRegExp = /-([a-z]{2})\.md$/
 
-/**
- * @param {string} string
- */
 function upperCaseFirst(string) {
   return `${string[0].toUpperCase()}${string.slice(1)}`
 }
 
-/**
- * @param {string} moduleID
- * @example moduleIDToJSIdentifier('./Box.js') === '$$IndexJs'
- * @example moduleIDToJSIdentifier('./Box-new.js') === '$$BoxNewJs'
- * @example moduleIDToJSIdentifier('../Box-new.js') === '$$$BoxNewJs'
- */
 function moduleIDToJSIdentifier(moduleID) {
   const delimiter = /(\.|-|\/|:)/
   return moduleID
@@ -26,12 +17,6 @@ function moduleIDToJSIdentifier(moduleID) {
     .map((part) => (part.length === 0 ? '$' : part))
     .map(upperCaseFirst)
     .join('')
-}
-
-const componentPackageMapping = {
-  'material-ui': {},
-  'base-ui': {},
-  'joy-ui': {},
 }
 
 module.exports = async function demoLoader() {
@@ -92,7 +77,6 @@ module.exports = async function demoLoader() {
   const {docs} = prepareMarkdown({
     fileRelativeContext,
     translations,
-    componentPackageMapping,
     options,
   })
 
@@ -118,8 +102,6 @@ module.exports = async function demoLoader() {
 
   await Promise.all(
     demoNames.map(async (demoName) => {
-      const multipleDemoVersionsUsed = !demoName.endsWith('.js')
-
       // TODO: const moduleID = demoName;
       // The import paths currently use a completely different format.
       // They should just use relative imports.
@@ -127,10 +109,6 @@ module.exports = async function demoLoader() {
         `pages/${fileRelativeContext.replace(/^docs\/src\/pages\//, '')}/`,
         '',
       )}`
-
-      if (multipleDemoVersionsUsed) {
-        moduleID = `${moduleID}/system/index.js`
-      }
 
       const moduleFilepath = path.join(
         path.dirname(this.resourcePath),
@@ -145,165 +123,6 @@ module.exports = async function demoLoader() {
       extractImports(demos[demoName].raw).forEach((importModuleID) =>
         importedModuleIDs.add(importModuleID),
       )
-
-      if (multipleDemoVersionsUsed) {
-        // Add Tailwind demo data
-        const tailwindModuleID = moduleID.replace(
-          '/system/index.js',
-          '/tailwind/index.js',
-        )
-        try {
-          // Add JS demo data
-          const tailwindModuleFilepath = path.join(
-            path.dirname(this.resourcePath),
-            tailwindModuleID.replace(/\//g, path.sep),
-          )
-
-          demos[demoName].moduleTailwind = tailwindModuleID
-          demos[demoName].rawTailwind = await fs.readFile(
-            tailwindModuleFilepath,
-            {
-              encoding: 'utf8',
-            },
-          )
-
-          this.addDependency(tailwindModuleFilepath)
-
-          demoModuleIDs.add(tailwindModuleID)
-
-          extractImports(demos[demoName].rawTailwind).forEach(
-            (importModuleID) => importedModuleIDs.add(importModuleID),
-          )
-
-          demoModuleIDs.add(demos[demoName].moduleTailwind)
-        } catch (error) {
-          // tailwind js demo doesn't exists
-        }
-
-        try {
-          // Add TS demo data
-          const tailwindTSModuleID = tailwindModuleID.replace('.js', '.tsx')
-
-          const tailwindTSModuleFilepath = path.join(
-            path.dirname(this.resourcePath),
-            tailwindTSModuleID.replace(/\//g, path.sep),
-          )
-
-          demos[demoName].moduleTSTailwind = tailwindTSModuleID
-          demos[demoName].rawTailwindTS = await fs.readFile(
-            tailwindTSModuleFilepath,
-            {
-              encoding: 'utf8',
-            },
-          )
-
-          this.addDependency(tailwindTSModuleFilepath)
-
-          demoModuleIDs.add(tailwindTSModuleID)
-
-          extractImports(demos[demoName].rawTailwindTS).forEach(
-            (importModuleID) => importedModuleIDs.add(importModuleID),
-          )
-
-          demoModuleIDs.add(demos[demoName].moduleTSTailwind)
-        } catch (error) {
-          // tailwind TS demo doesn't exists
-        }
-
-        // Add plain CSS demo data
-        const cssModuleID = moduleID.replace(
-          '/system/index.js',
-          '/css/index.js',
-        )
-        try {
-          // Add JS demo data
-          const cssModuleFilepath = path.join(
-            path.dirname(this.resourcePath),
-            cssModuleID.replace(/\//g, path.sep),
-          )
-
-          demos[demoName].moduleCSS = cssModuleID
-          demos[demoName].rawCSS = await fs.readFile(cssModuleFilepath, {
-            encoding: 'utf8',
-          })
-
-          this.addDependency(cssModuleFilepath)
-
-          demoModuleIDs.add(cssModuleID)
-
-          extractImports(demos[demoName].rawCSS).forEach((importModuleID) =>
-            importedModuleIDs.add(importModuleID),
-          )
-
-          demoModuleIDs.add(demos[demoName].moduleCSS)
-        } catch (error) {
-          // plain css js demo doesn't exists
-        }
-
-        try {
-          // Add TS demo data
-          const cssTSModuleID = cssModuleID.replace('.js', '.tsx')
-
-          const cssTSModuleFilepath = path.join(
-            path.dirname(this.resourcePath),
-            cssTSModuleID.replace(/\//g, path.sep),
-          )
-
-          demos[demoName].moduleTSCSS = cssTSModuleID
-          demos[demoName].rawCSSTS = await fs.readFile(cssTSModuleFilepath, {
-            encoding: 'utf8',
-          })
-
-          this.addDependency(cssTSModuleFilepath)
-
-          demoModuleIDs.add(cssTSModuleID)
-
-          extractImports(demos[demoName].rawCSSTS).forEach((importModuleID) =>
-            importedModuleIDs.add(importModuleID),
-          )
-
-          demoModuleIDs.add(demos[demoName].moduleTSCSS)
-        } catch (error) {
-          // plain css demo doesn't exists
-        }
-
-        // Tailwind preview
-        try {
-          const tailwindPreviewFilepath = moduleFilepath.replace(
-            `${path.sep}system${path.sep}index.js`,
-            `${path.sep}tailwind${path.sep}index.tsx.preview`,
-          )
-
-          const tailwindJsxPreview = await fs.readFile(
-            tailwindPreviewFilepath,
-            {
-              encoding: 'utf8',
-            },
-          )
-          this.addDependency(tailwindPreviewFilepath)
-
-          demos[demoName].tailwindJsxPreview = tailwindJsxPreview
-        } catch (error) {
-          // No preview exists. This is fine.
-        }
-
-        // CSS preview
-        try {
-          const cssPreviewFilepath = moduleFilepath.replace(
-            `${path.sep}system${path.sep}index.js`,
-            `${path.sep}css${path.sep}index.tsx.preview`,
-          )
-
-          const cssJsxPreview = await fs.readFile(cssPreviewFilepath, {
-            encoding: 'utf8',
-          })
-          this.addDependency(cssPreviewFilepath)
-
-          demos[demoName].cssJsxPreview = cssJsxPreview
-        } catch (error) {
-          // No preview exists. This is fine.
-        }
-      }
 
       try {
         const previewFilepath = moduleFilepath.replace(/\.js$/, '.tsx.preview')
