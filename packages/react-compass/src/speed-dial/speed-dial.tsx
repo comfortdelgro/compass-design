69694e2ee1b828891ed3fbd43b13fb8135fbbd9b
michael-le-19 <117263060+michael-le-19@users.noreply.github.com>
2023-07-17 03:34:08 +0700

    [CDS-427] Add accessibility to speed dial (#905)
    * feature: add a11y to speed dial
    
    * feature: create keyboard nav hooks
    
    * feature: improve keyboard hook
    
    * feature: improve keyboard hook
    
    * feature: add generic to keyboard hook
    
    * feature: allow disabled item in keyboard hook
    
    * feature: add toggle open when click speed dial
    
    * feature: remove redundant children

--- packages/react-compass/src/speed-dial/speed-dial.tsx
+++ packages/react-compass/src/speed-dial/speed-dial.tsx
@@ -1,16 +1,19 @@
 import {faPlus} from '@fortawesome/free-solid-svg-icons/faPlus'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import React, {useState} from 'react'
+import {
+  KeyboardNavigationProvider,
+  useKeyboardNavigation,
+  useKeyboardNavigationState,
+} from '../utils/hooks'
 import {StyledComponentProps} from '../utils/stitches.types'
 import {useDOMRef} from '../utils/use-dom-ref'
+import {SpeedDialAction} from './speed-dial-action'
+import {SpeedDialTrigger} from './speed-dial-trigger'
 import {
-  SpeedDialAction,
-  SpeedDialIcon,
-  SpeedDialName,
   SpeedDialVariantProps,
   StyledSpeedDial,
   StyledSpeedDialActions,
-  StyledSpeedDialTrigger,
   StyledSpeedDialTriggerContent,
 } from './speed-dial.styles'
 
@@ -40,17 +43,27 @@ const SpeedDial = React.forwardRef<HTMLDivElement, SpeedDialProps>(
 
     const [isOpen, setIsOpen] = useState(false)
 
+    const toggleOpen = () => {
+      setIsOpen((prev) => !prev)
+    }
+
     const handleMouseEnter = () => {
       setIsOpen(true)
     }
 
     const handleMouseLeave = () => {
       setIsOpen(false)
+      handleClose()
     }
 
     const handleActionClick = (action: () => void) => {
-      setIsOpen(false)
       action()
+      handleClose()
+    }
+
+    const handleClose = () => {
+      setIsOpen(false)
+      resetFocus?.()
     }
 
     const componentProps = () => {
@@ -61,45 +74,82 @@ const SpeedDial = React.forwardRef<HTMLDivElement, SpeedDialProps>(
       }
     }
 
+    const {onKeyDown, nextFocus, prevFocus, resetFocus} =
+      useKeyboardNavigation()
+
+    const handleKeyDown = onKeyDown?.<HTMLDivElement>({
+      ArrowUp: (e) => {
+        e.preventDefault()
+        nextFocus?.()
+      },
+      ArrowDown: (e) => {
+        e.preventDefault()
+        prevFocus?.()
+      },
+      Tab: () => {
+        handleClose()
+      },
+      Escape: () => {
+        handleClose()
+      },
+    })
+
     const delegateProps = componentProps()
+
     return (
       <StyledSpeedDial
         {...delegateProps}
         ref={speedDialRef}
         className={`speed-dial speed-dial--${position || 'up'} ${position}`}
+        role='presentation'
         onMouseLeave={handleMouseLeave}
+        onKeyDown={handleKeyDown}
       >
-        <StyledSpeedDialTrigger
+        <SpeedDialTrigger
           className={`speed-dial__trigger ${isOpen ? 'open' : ''}`}
           onMouseEnter={handleMouseEnter}
+          onFocus={handleMouseEnter}
+          onClick={toggleOpen}
+          aria-expanded={isOpen}
+          aria-haspopup={true}
         >
           <StyledSpeedDialTriggerContent>
             <FontAwesomeIcon icon={faPlus} />
           </StyledSpeedDialTriggerContent>
-        </StyledSpeedDialTrigger>
+        </SpeedDialTrigger>
         <StyledSpeedDialActions
           className={`speed-dial__actions speed-dial__actions--${
             position || 'up'
           } ${isOpen ? 'open' : ''}`}
           isOpenAction={isOpen}
+          role='menu'
         >
           {actions.map(({name, icon, onClick}, index) => (
             <SpeedDialAction
               key={index}
-              className={`speed-dial__action speed-dial__action--${position}`}
               onClick={() => handleActionClick(onClick)}
-            >
-              <SpeedDialIcon className='speed-dial__action-icon'>
-                {icon}
-              </SpeedDialIcon>
-              <SpeedDialName className='speed-dial__action-name'>
-                {name}
-              </SpeedDialName>
-            </SpeedDialAction>
+              icon={icon}
+              name={name}
+              position={position}
+            />
           ))}
         </StyledSpeedDialActions>
       </StyledSpeedDial>
     )
   },
 )
-export default SpeedDial
+
+export const SpeedDialContextWrapper = React.forwardRef<
+  HTMLDivElement,
+  SpeedDialProps
+>((props, ref) => {
+  const {provider} = useKeyboardNavigationState()
+
+  return (
+    <KeyboardNavigationProvider {...provider}>
+      <SpeedDial {...props} ref={ref} />
+    </KeyboardNavigationProvider>
+  )
+})
+
+export default SpeedDialContextWrapper
