2e572b2e4a258df2c535a2c31a49eb3589f97f95
Tai Bui <tai.c.bui@gmail.com>
2023-07-19 13:27:17 +0700

    Finish

--- packages/react-compass/src/calendar/index.ts
+++ packages/react-compass/src/calendar/index.ts
@@ -1,5 +1,5 @@
 import * as InternationalizedDate from '@internationalized/date'
-import * as i18n from '@react-aria/i18n'
+import * as i18n from './../internationalized/i18n'
 import Calendar from './calendar'
 
 export type {CalendarProps} from './calendar'
