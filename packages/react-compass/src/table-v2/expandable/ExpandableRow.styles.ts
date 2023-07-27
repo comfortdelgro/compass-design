97464a49ba8c5f32f26dc68aa3de8698337a6837
Tai Bui <tai.c.bui@gmail.com>
2023-07-27 17:16:12 +0700

    fix

--- packages/react-compass/src/table-v2/expandable/ExpandableRow.styles.ts
+++ packages/react-compass/src/table-v2/expandable/ExpandableRow.styles.ts
@@ -3,6 +3,7 @@ import type {VariantProps} from '../../utils/stitches.types'
 
 export const StyledExpandableRow = styled('tr', {
   all: 'unset',
+  height: 'fit-content',
 })
 
 export type ExpandableRowProps = VariantProps<typeof StyledExpandableRow>
--- packages/react-compass/src/table-v2/expandable/ExpandableRow.tsx
+++ packages/react-compass/src/table-v2/expandable/ExpandableRow.tsx
@@ -19,11 +19,27 @@ export const ExpandableRow = React.forwardRef<
   const {colSpan, isExpanded, children} = props
 
   const rowRef = useDOMRef<HTMLTableRowElement>(ref)
+  const [childrenHeight, setChildrenHeight] = React.useState<number>(1000)
+
   React.useEffect(() => {
-    console.log('isExpaned', isExpanded)
+    if (isExpanded) {
+      // Measure the height of the children content
+      const tdElement = rowRef.current?.querySelector('td')
+      if (tdElement) {
+        const height = tdElement.clientHeight
+        setChildrenHeight(height)
+      }
+    }
   }, [isExpanded])
+
   return (
-    <Transitions effect='collapse' show={isExpanded} speed={0.5}>
+    <Transitions
+      className='cdg-transition'
+      effect='collapse'
+      show={isExpanded}
+      speed={0.5}
+      collapsedSize={`${childrenHeight}px`}
+    >
       <StyledExpandableRow ref={rowRef}>
         <td colSpan={colSpan}>{children}</td>
       </StyledExpandableRow>
