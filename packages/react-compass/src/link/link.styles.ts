819bc20894c42c94b68051bbd90f6e3f02431da2
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-07-27 22:10:18 +0700

    [yagin][add width and role for link]

--- packages/react-compass/src/link/link.styles.ts
+++ packages/react-compass/src/link/link.styles.ts
@@ -2,6 +2,7 @@ import {styled} from '../theme'
 import type {VariantProps} from '../utils/stitches.types'
 
 export const StyledLink = styled('a', {
+  width: 'fit-content',
   fontFamily: 'inherit',
   fontWeight: '$medium',
   color: '$info',
