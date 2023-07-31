7738251a659293a6454e26f8c0df27cd0fc95167
Duong Tran Xuan <119040724+fpt-duongtx@users.noreply.github.com>
2023-07-31 17:10:41 +0700

    [CDS-443] Feature/portal layout (#977)
    * Init layout files
    
    * Add portal demo
    
    * Update portal layout
    
    * Add layout sample
    
    * Add alignment sample
    
    ---------
    
    Co-authored-by: fpt-duongtx <duongtx@fpt.com>

---
+++ packages/react-compass/src/layouts/Layout.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {CSS, StyledComponentProps} from '../utils/stitches.types'
+import {useDOMRef} from '../utils/use-dom-ref'
+import {StyledLayout} from './Layout.styles'
+
+export type LayoutAlignment =
+  | 'start-start'
+  | 'center-start'
+  | 'end-start'
+  | 'start-center'
+  | 'center-center'
+  | 'end-center'
+  | 'start-end'
+  | 'center-end'
+  | 'end-end'
+
+interface Props extends StyledComponentProps {
+  children?: React.ReactNode
+  css?: CSS
+  direction?: 'row' | 'column'
+  flex?: Number
+  align?: LayoutAlignment
+}
+
+export type LayoutProps = Props &
+  Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
+
+export const Layout = React.forwardRef<HTMLDivElement, LayoutProps>(
+  (props, ref) => {
+    const layoutRef = useDOMRef<HTMLDivElement>(ref)
+    const {
+      children,
+      direction = 'column',
+      align,
+      flex,
+      css = {},
+      ...delegated
+    } = props
+
+    return (
+      <StyledLayout
+        ref={layoutRef}
+        css={css}
+        {...delegated}
+        className={`cdg-layout ${direction} ${align ? ' ' + align : ''}`}
+        style={flex ? {flex: flex.toString()} : {}}
+      >
+        {children}
+      </StyledLayout>
+    )
+  },
+)
+
+export default Layout as typeof Layout & {}
