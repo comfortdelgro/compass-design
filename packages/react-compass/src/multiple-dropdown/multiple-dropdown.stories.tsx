e8b18d7862f609aa04221185c700604395680b92
fpt-tom-du <tudq3@fsoft.com.vn>
2023-08-02 22:30:30 +0700

    Add popoverCSS Dropdown & MultipleDropdown

--- packages/react-compass/src/multiple-dropdown/multiple-dropdown.stories.tsx
+++ packages/react-compass/src/multiple-dropdown/multiple-dropdown.stories.tsx
@@ -500,6 +500,26 @@ export const Default: React.FC = () => {
           </MultipleDropdown.Item>
           <MultipleDropdown.Item value='snake'>Snake</MultipleDropdown.Item>
         </MultipleDropdown>
+        <h3>Limit width for Popover</h3>
+        <MultipleDropdown
+          label='Favorite Animal'
+          placeholder='Animals'
+          css={{width: 250}}
+          popoverCSS={{width: 'auto'}}
+        >
+          <MultipleDropdown.Item value='Panda' textValue='Panda'>
+            Red Panda Red Panda Red Panda Red Panda Red Panda
+          </MultipleDropdown.Item>
+          <MultipleDropdown.Item value='cat'>Cat</MultipleDropdown.Item>
+          <MultipleDropdown.Item value='dog'>Dog</MultipleDropdown.Item>
+          <MultipleDropdown.Item value='aardvark'>
+            Aardvark
+          </MultipleDropdown.Item>
+          <MultipleDropdown.Item value='kangaroo'>
+            Kangaroo
+          </MultipleDropdown.Item>
+          <MultipleDropdown.Item value='snake'>Snake</MultipleDropdown.Item>
+        </MultipleDropdown>
         <h3>Load more</h3>
         <MultipleDropdown
           label='Using load more'
