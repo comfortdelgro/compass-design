c08ad610160b5aa315f8745a42ed6614e25dc908
hoapn-fsoft <128688086+hoapn-fsoft@users.noreply.github.com>
2023-08-17 16:29:15 +0700

    [NGPDCP-2594] Add new variant sub header component for h5 (#1030)
    * feat: add sub header for h5
    
    * chore: remove important unuse
    
    * fix: import variant props

---
+++ packages/react-compass/src/sub-header/sub-header-image.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {StyledComponentProps} from '../utils/stitches.types'
+import {StyledSubHeaderImage} from './sub-header-image.style'
+
+export type SubHeaderImageProps = StyledComponentProps &
+  Omit<React.ImgHTMLAttributes<HTMLImageElement>, keyof StyledComponentProps>
+
+const SubHeaderImage = React.forwardRef<HTMLImageElement, SubHeaderImageProps>(
+  (props, ref) => {
+    const {css = {}, ...delegated} = props
+
+    return <StyledSubHeaderImage ref={ref} css={css} {...delegated} />
+  },
+)
+
+export default SubHeaderImage
--- packages/react-compass/src/sub-header/sub-header-title.tsx
+++ packages/react-compass/src/sub-header/sub-header-title.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import {StyledComponentProps} from '../utils/stitches.types'
-import {StyledSubHeaderTitle} from './sub-header-title.style'
+import {
+  StyledSubHeaderTitle,
+  SubHeaderTitleVariantProps,
+} from './sub-header-title.style'
 
 interface Props extends StyledComponentProps {
   children: React.ReactNode
+  variant?: SubHeaderTitleVariantProps['variant']
 }
 
 export type SubHeaderTitleProps = Props &
@@ -11,10 +15,15 @@ export type SubHeaderTitleProps = Props &
 
 const SubHeaderTitle = React.forwardRef<HTMLDivElement, SubHeaderTitleProps>(
   (props, ref) => {
-    const {children, css = {}, ...delegated} = props
+    const {children, css = {}, variant = 'default', ...delegated} = props
 
     return (
-      <StyledSubHeaderTitle ref={ref} css={css} {...delegated}>
+      <StyledSubHeaderTitle
+        ref={ref}
+        css={css}
+        {...delegated}
+        variant={variant}
+      >
         {children}
       </StyledSubHeaderTitle>
     )
