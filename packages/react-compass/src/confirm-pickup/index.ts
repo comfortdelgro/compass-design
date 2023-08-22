6b97cb7f448e7a7494049447cf50cfbbbaea13db
Hai Do Tien <dotienhai1611@gmail.com>
2023-08-22 13:16:22 +0700

    [NGPDC-2597] confirm PU point component (#1041)
    * add skeleton for component
    
    * update PU point component
    
    * add some styling
    
    * Fix pr review comment
    
    * add placeholder in the example

---
+++ packages/react-compass/src/confirm-pickup/index.ts
@@ -0,0 +1,6 @@
+import ConfirmPUPoint, {IPickupPoint} from './confirm-pickup'
+export type {ConfirmPUPointProps} from './confirm-pickup'
+
+export type PickupPointType = IPickupPoint
+ConfirmPUPoint.displayName = 'ConfirmPUPoint'
+export default ConfirmPUPoint
--- packages/react-compass/src/index.ts
+++ packages/react-compass/src/index.ts
@@ -66,6 +66,8 @@ export {default as Checkbox} from './checkbox'
 export type {CheckboxProps} from './checkbox'
 export {default as Chip} from './chip'
 export type {ChipProps} from './chip'
+export {default as ConfirmPUPoint} from './confirm-pickup'
+export type {ConfirmPUPointProps, PickupPointType} from './confirm-pickup'
 export {default as DashboardSidecard} from './dashboard-sidecard'
 export type {
   DashboardSidecardContentProps,
