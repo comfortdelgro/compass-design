947fb808ae242846fb0d202b3f058f4587735302
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-08-25 16:46:36 +0700

    feat: add new icon for docs v2

--- apps/docs-v2/data/foundation/iconography/showcases/ColorfulIcons.tsx
+++ apps/docs-v2/data/foundation/iconography/showcases/ColorfulIcons.tsx
@@ -1,4 +1,12 @@
+import AdminPotalColorful from '@comfortdelgro/compass-icons/react/colorful/admin-potal'
+import AdminPotalLightColorful from '@comfortdelgro/compass-icons/react/colorful/admin-potal-light'
+import CabyPortalColorful from '@comfortdelgro/compass-icons/react/colorful/caby-portal'
+import CabyPortalLightColorful from '@comfortdelgro/compass-icons/react/colorful/caby-portal-light'
 import ClubhouseColorful from '@comfortdelgro/compass-icons/react/colorful/clubhouse'
+import CmsColorful from '@comfortdelgro/compass-icons/react/colorful/cms'
+import CmsLightColorful from '@comfortdelgro/compass-icons/react/colorful/cms-light'
+import ComfortConnectColorful from '@comfortdelgro/compass-icons/react/colorful/comfort-connect'
+import ComfortConnectLightColorful from '@comfortdelgro/compass-icons/react/colorful/comfort-connect-light'
 import CompassColorful from '@comfortdelgro/compass-icons/react/colorful/compass'
 import CrownColorful from '@comfortdelgro/compass-icons/react/colorful/crown'
 import DiscordColorful from '@comfortdelgro/compass-icons/react/colorful/discord'
@@ -6,6 +14,10 @@ import DoctypeColorful from '@comfortdelgro/compass-icons/react/colorful/doctype
 import DribbbleColorful from '@comfortdelgro/compass-icons/react/colorful/dribbble'
 import FacebookColorful from '@comfortdelgro/compass-icons/react/colorful/facebook'
 import FigmaColorful from '@comfortdelgro/compass-icons/react/colorful/figma'
+import FmsColorful from '@comfortdelgro/compass-icons/react/colorful/fms'
+import FmsLightColorful from '@comfortdelgro/compass-icons/react/colorful/fms-light'
+import FnbColorful from '@comfortdelgro/compass-icons/react/colorful/fnb'
+import FnbLightColorful from '@comfortdelgro/compass-icons/react/colorful/fnb-light'
 import GoogleColorful from '@comfortdelgro/compass-icons/react/colorful/google'
 import LinkedinColorful from '@comfortdelgro/compass-icons/react/colorful/linkedin'
 import PinterestColorful from '@comfortdelgro/compass-icons/react/colorful/pinterest'
@@ -13,16 +25,21 @@ import RedditColorful from '@comfortdelgro/compass-icons/react/colorful/reddit'
 import SignalColorful from '@comfortdelgro/compass-icons/react/colorful/signal'
 import SnapchatColorful from '@comfortdelgro/compass-icons/react/colorful/snapchat'
 import TelegramColorful from '@comfortdelgro/compass-icons/react/colorful/telegram'
+import TemColorful from '@comfortdelgro/compass-icons/react/colorful/tem'
+import TemLightColorful from '@comfortdelgro/compass-icons/react/colorful/tem-light'
 import TiktokColorful from '@comfortdelgro/compass-icons/react/colorful/tiktok'
 import TumblrColorful from '@comfortdelgro/compass-icons/react/colorful/tumblr'
 import TwitterColorful from '@comfortdelgro/compass-icons/react/colorful/twitter'
 import WorkbenchColorful from '@comfortdelgro/compass-icons/react/colorful/workbench'
+import WorkbenchLightColorful from '@comfortdelgro/compass-icons/react/colorful/workbench-light'
 import WorkbenchTextColorful from '@comfortdelgro/compass-icons/react/colorful/workbench-text'
 import YoutubeColorful from '@comfortdelgro/compass-icons/react/colorful/youtube'
 import ZigBizColorful from '@comfortdelgro/compass-icons/react/colorful/zig-biz'
 import ZigBizSquareColorful from '@comfortdelgro/compass-icons/react/colorful/zig-biz-square'
 import ZigBizTsmColorful from '@comfortdelgro/compass-icons/react/colorful/zig-biz-tsm'
 import ZigBizWhiteColorful from '@comfortdelgro/compass-icons/react/colorful/zig-biz-white'
+import ZigStoreColorful from '@comfortdelgro/compass-icons/react/colorful/zig-store'
+import ZigStoreLightColorful from '@comfortdelgro/compass-icons/react/colorful/zig-store-light'
 import {Flexbox} from '@comfortdelgro/react-compass'
 import IconBox from 'data/foundation/iconography/components/IconBox'
 
@@ -36,8 +53,37 @@ export default function colorfulIcons() {
       }}
     >
       <IconBox
+        component={<AdminPotalLightColorful width={40} height={40} />}
+        name='AdminPotalLight'
+      />
+      <IconBox
+        component={<AdminPotalColorful width={40} height={40} />}
+        name='AdminPotal'
+      />
+      <IconBox
+        component={<CabyPortalLightColorful width={40} height={40} />}
+        name='CabyPortalLight'
+      />
+      <IconBox
+        component={<CabyPortalColorful width={40} height={40} />}
+        name='CabyPortal'
+      />
+      <IconBox
         component={<ClubhouseColorful width={40} height={40} />}
         name='Clubhouse'
+      />
+      <IconBox
+        component={<CmsLightColorful width={40} height={40} />}
+        name='CmsLight'
+      />
+      <IconBox component={<CmsColorful width={40} height={40} />} name='Cms' />
+      <IconBox
+        component={<ComfortConnectLightColorful width={40} height={40} />}
+        name='ComfortConnectLight'
+      />
+      <IconBox
+        component={<ComfortConnectColorful width={40} height={40} />}
+        name='ComfortConnect'
       />
       <IconBox
         component={<CompassColorful width={40} height={40} />}
@@ -68,6 +114,16 @@ export default function colorfulIcons() {
         name='Figma'
       />
       <IconBox
+        component={<FmsLightColorful width={40} height={40} />}
+        name='FmsLight'
+      />
+      <IconBox component={<FmsColorful width={40} height={40} />} name='Fms' />
+      <IconBox
+        component={<FnbLightColorful width={40} height={40} />}
+        name='FnbLight'
+      />
+      <IconBox component={<FnbColorful width={40} height={40} />} name='Fnb' />
+      <IconBox
         component={<GoogleColorful width={40} height={40} />}
         name='Google'
       />
@@ -96,6 +152,11 @@ export default function colorfulIcons() {
         name='Telegram'
       />
       <IconBox
+        component={<TemLightColorful width={40} height={40} />}
+        name='TemLight'
+      />
+      <IconBox component={<TemColorful width={40} height={40} />} name='Tem' />
+      <IconBox
         component={<TiktokColorful width={40} height={40} />}
         name='Tiktok'
       />
@@ -106,6 +167,10 @@ export default function colorfulIcons() {
       <IconBox
         component={<TwitterColorful width={40} height={40} />}
         name='Twitter'
+      />
+      <IconBox
+        component={<WorkbenchLightColorful width={40} height={40} />}
+        name='WorkbenchLight'
       />
       <IconBox
         component={<WorkbenchTextColorful width={40} height={40} />}
@@ -134,6 +199,14 @@ export default function colorfulIcons() {
       <IconBox
         component={<ZigBizColorful width={40} height={40} />}
         name='ZigBiz'
+      />
+      <IconBox
+        component={<ZigStoreLightColorful width={40} height={40} />}
+        name='ZigStoreLight'
+      />
+      <IconBox
+        component={<ZigStoreColorful width={40} height={40} />}
+        name='ZigStore'
       />
     </Flexbox>
   )
