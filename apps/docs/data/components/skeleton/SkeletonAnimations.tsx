79579a7b6cb3442ea9aa86bca19e257a590ac420
yagin94 <50587656+yagin94@users.noreply.github.com>
2023-09-10 10:18:49 +0700

    [skeleton] support dark theme (#1124)
    Co-authored-by: Nguyen Duy Hien <nguyenduy.hien@petronas.com>

--- apps/docs/data/components/skeleton/SkeletonAnimations.tsx
+++ apps/docs/data/components/skeleton/SkeletonAnimations.tsx
@@ -3,9 +3,19 @@ import {Skeleton} from '@comfortdelgro/react-compass'
 function SkeletonAnimations() {
   return (
     <div style={{gap: '1rem', display: 'flex', flexDirection: 'column'}}>
-      <Skeleton width={210} height={60} />
-      <Skeleton width={210} height={60} animation='wave' />
-      <Skeleton width={210} height={60} animation={false} />
+      <Skeleton width={210} height={60} color='var(--colors-gray40)' />
+      <Skeleton
+        width={210}
+        height={60}
+        animation='wave'
+        color='var(--colors-gray40)'
+      />
+      <Skeleton
+        width={210}
+        height={60}
+        animation={false}
+        color='var(--colors-gray40)'
+      />
     </div>
   )
 }
