86aadc5744f4cde92808c8360113d705d27eb85c
fpt-duongtx <duongtx@fpt.com>
2023-09-11 22:04:31 +0700

    Fix card shadow on hover

--- packages/react-compass/src/card/card.stories.tsx
+++ packages/react-compass/src/card/card.stories.tsx
@@ -101,6 +101,32 @@ export const Variants: React.FC = () => {
           </Card.Action>
         </Card.Body>
       </Card>
+      <h3>Default - Shadowless</h3>
+      <Card size='lg' isShadowless={true}>
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <p style={{margin: 0}}>{lorem.slice(0, lorem.length / 2)}</p>
+          <Card.Action>
+            <Button variant='primary'>Button</Button>
+            <Button variant='secondary'>Button</Button>
+          </Card.Action>
+        </Card.Body>
+      </Card>
+      <h3>Disabled - Shadowless</h3>
+      <Card isDisabled={true} size='lg' isShadowless={true}>
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <p style={{margin: 0}}>{lorem.slice(0, lorem.length / 2)}</p>
+          <Card.Action>
+            <Button isDisabled={isDisabled} variant='primary'>
+              Button
+            </Button>
+            <Button isDisabled={isDisabled} variant='secondary'>
+              Button
+            </Button>
+          </Card.Action>
+        </Card.Body>
+      </Card>
     </Column>
   )
 }
