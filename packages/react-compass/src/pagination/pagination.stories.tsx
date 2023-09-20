77f3594396840a392e7d54f980c56ec39341b6fe
yagin94 <50587656+yagin94@users.noreply.github.com>
2023-09-20 08:16:55 +0700

    [CDS-717] Pagination missing "Item counting" component (#1117)
    * [pagination][add Items Counter]
    
    * [pagination][fix merge conflict]
    
    * [pagination][add rows per page]
    
    ---------
    
    Co-authored-by: Nguyen Duy Hien <nguyenduy.hien@petronas.com>

--- packages/react-compass/src/pagination/pagination.stories.tsx
+++ packages/react-compass/src/pagination/pagination.stories.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Column} from '../utils'
 import Pagination from './index'
 
 export const Variants = () => {
+  const [page, setPage] = useState(1)
   return (
     <Column>
       <h3>1. Total pages prop</h3>
@@ -11,11 +12,16 @@ export const Variants = () => {
       <Pagination total={1000000000} page={3} />
       <h3>3. OnChange prop</h3>
       <Pagination
-        total={1000000000}
-        onChange={(page) => {
-          console.log(page)
+        rowsOptions={[5, 10, 15, 20]}
+        rowsPerPage={5}
+        onRowsPerPageChange={(rows) => {
+          console.log(rows)
         }}
-      />
+        page={page}
+        total={10}
+        onChange={(page) => setPage(page)}
+        count={100}
+      ></Pagination>
     </Column>
   )
 }
