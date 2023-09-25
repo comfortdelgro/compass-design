dab6386c5937556a2a9c67c09964fb60b7636422
yagin94 <50587656+yagin94@users.noreply.github.com>
2023-09-25 15:00:21 +0700

    [CDS-848] Table V2 - Editable cell border styles (#1199)
    * [table][fix styles follow figma]
    
    * [pagination] add disable first or last page
    
    * [pagination]change disable color and cursor
    
    ---------
    
    Co-authored-by: Nguyen Duy Hien <nguyenduy.hien@petronas.com>

--- packages/react-compass/src/pagination/pagination.tsx
+++ packages/react-compass/src/pagination/pagination.tsx
@@ -195,7 +195,7 @@ const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>(
         >
           <svg viewBox='0 0 320 512'>
             <path
-              fill={active === 1 ? '#EDEBE9' : '#201F1E'}
+              fill={active === 1 ? '#D2D0CE' : '#201F1E'}
               d='M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z'
             ></path>
           </svg>
@@ -208,7 +208,7 @@ const Pagination = React.forwardRef<HTMLDivElement, PaginationProps>(
         >
           <svg viewBox='0 0 320 512'>
             <path
-              fill={active === total ? '#EDEBE9' : '#201F1E'}
+              fill={active === total ? '#D2D0CE' : '#201F1E'}
               d='M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z'
             ></path>
           </svg>
