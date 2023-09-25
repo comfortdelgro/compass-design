48514d3c9a3213e7d3f7eed944e29d6333fad92b
yagin94 <50587656+yagin94@users.noreply.github.com>
2023-09-25 14:38:22 +0700

    [CDS-840] Table V2 - Table row - some styles mismatch with the design (#1197)
    * [table][fix styles follow figma]
    
    * [pagination] add disable first or last page
    
    ---------
    
    Co-authored-by: Nguyen Duy Hien <nguyenduy.hien@petronas.com>

--- packages/react-compass/src/table-v2/table-v2-checkbox-cell.styles.ts
+++ packages/react-compass/src/table-v2/table-v2-checkbox-cell.styles.ts
@@ -3,5 +3,6 @@ import {styled} from '../theme'
 export const StyledDataGridCheckboxCell = styled('span', {
   textAlign: 'left',
   fontSize: '$label1',
-  padding: '$3_5 $3',
+  padding: '$4 $3',
+  lineHeight: '$5_25',
 })
