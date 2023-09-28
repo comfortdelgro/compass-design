d103f7ff39fd064cb8c51984978aa3346d898bc3
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-09-28 10:56:05 +0700

    feature: CDS-893

---
+++ packages/static/src/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,65 @@
+/* eslint-disable react-refresh/only-export-components */
+import React from 'react'
+import CssInjection from '../utils/objectToCss/CssInjection'
+import {useDOMRef} from '../utils/use-dom-ref'
+import styles from './styles/breadscrumbs.module.css'
+import BreadcrumbItem from './breadcrumb-item'
+
+export interface Props {
+  css?: unknown
+  children?: React.ReactNode
+  dividerIcon?: React.ReactNode
+  isCurrent?: (item: number) => boolean
+}
+
+
+export type BreadcrumbsProps = Props &
+  Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
+
+const Breadcrumbs = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
+  const {
+    css = {},
+    dividerIcon = (
+      <svg viewBox='0 0 320 512'>
+        <path
+          fill='currentColor'
+          d='M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z'
+        />
+      </svg>
+    ),
+    isCurrent,
+    children,
+    ...delegated
+  } = props
+
+  const breadcrumbsRef = useDOMRef<HTMLDivElement>(ref)
+
+  const items = React.Children.toArray(children)
+
+  return (
+    <CssInjection css={css} childrenRef={breadcrumbsRef}>
+      <nav
+        ref={breadcrumbsRef}
+        className={styles['cdg-breadcrumbs']}
+        {...delegated}
+      >
+        <ol>
+          {items.map((item, i) => (
+            <React.Fragment key={i}>
+              {React.cloneElement(item as JSX.Element, {
+                isCurrent: isCurrent ? isCurrent(i) : false,
+              })}
+              {i < items.length - 1 && (
+                <li aria-hidden className={styles.divider }>
+                  {dividerIcon}
+                </li>
+              )}
+            </React.Fragment>
+          ))}
+        </ol>
+      </nav>
+    </CssInjection>
+  )
+})
+
+export default Breadcrumbs as typeof Breadcrumbs & {Item: typeof BreadcrumbItem}
