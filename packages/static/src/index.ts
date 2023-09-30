5b888dd2aa9609bae511cb36dde70c0f4804544d
TuDQ <quoctu2014@gmail.com>
2023-10-01 01:59:47 +0700

    Add static css for MultipleDropdown

--- packages/static/src/index.ts
+++ packages/static/src/index.ts
@@ -3,3 +3,9 @@ export type {ChipProps} from './chip'
 export {default as Popover} from './popover'
 export type {PopoverDirection, PopoverProps} from './popover'
 export {default as ThemeStaticProvider} from './theme-static/ThemeProvider'
+
+export {default as MultipleDropdown} from './multiple-dropdown'
+export type {
+  MultipleDropdownItemProps,
+  MultipleDropdownProps,
+} from './multiple-dropdown'
