1bec86c801b7a57e780d02c127e83ac5529d935a
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-10-06 10:06:09 +0700

    [searchfield] change static part1

---
+++ packages/static/src/searchfield/searchfield.stories.tsx
@@ -0,0 +1,105 @@
+import type {StoryDecorator} from '@ladle/react'
+import React from 'react'
+import {Column} from '../utils/components'
+import SearchField from './index'
+
+export const Variants: React.FC = () => {
+  const [value, setValue] = React.useState('My initial value')
+  return (
+    <Column>
+      <h3>Basic</h3>
+      <SearchField
+        placeholder='Search'
+        onSubmit={(text) => console.log(text)}
+        value={'Initial value'}
+        css={{width: '10rem'}}
+      />
+
+      <h3>isErrored</h3>
+      <SearchField
+        label='SearchField isErrored = true'
+        placeholder='Search'
+        onSubmit={(text) => console.log(text)}
+        value={'Initial value'}
+        isErrored={true}
+        css={{width: '10rem'}}
+        errorMessage='SearchField error message'
+      />
+
+      <h3>isDisabled</h3>
+      <SearchField
+        isDisabled
+        placeholder='Search'
+        css={{width: '10rem'}}
+        onSubmit={(text) => console.log(text)}
+      />
+      <h3>isReadOnly</h3>
+      <SearchField
+        isReadOnly
+        placeholder='Search'
+        css={{width: '10rem'}}
+        onSubmit={(text) => console.log(text)}
+      />
+
+      <h3>Controlled</h3>
+      <SearchField
+        placeholder='Search'
+        value={value}
+        onChange={(value) => setValue(value)}
+        css={{width: '10rem'}}
+      />
+      <h3> Max Length</h3>
+      <SearchField
+        placeholder='Search'
+        maxLength={10}
+        css={{width: '10rem'}}
+        onSubmit={(text) => console.log(text)}
+      />
+      <h3> Min Length</h3>
+      <SearchField
+        placeholder='Search'
+        minLength={5}
+        css={{width: '10rem'}}
+        onSubmit={(text) => console.log(text)}
+      />
+
+      <h3> H5</h3>
+      <SearchField
+        placeholder='Search'
+        minLength={5}
+        css={{
+          width: '343px',
+        }}
+        h5
+        onSubmit={(text) => console.log(text)}
+      />
+
+      <h3> H5 Searchfield with box shadow</h3>
+      <SearchField
+        placeholder='Search'
+        minLength={5}
+        css={{
+          width: '343px',
+          boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 4px;',
+        }}
+        h5
+        onSubmit={(text) => console.log(text)}
+      />
+    </Column>
+  )
+}
+
+export default {
+  decorators: [
+    (Component) => (
+      <div>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `.ladle-main { background: #eee; }`,
+          }}
+        ></style>
+        <Component />
+      </div>
+    ),
+  ] as StoryDecorator[],
+}
---
+++ packages/static/src/searchfield/searchfield.styles.ts
@@ -0,0 +1,164 @@
+import {StyledButton} from '../button/button.styles'
+import {styled} from '../theme'
+import type {VariantProps} from '../utils/stitches.types'
+
+export const StyledSearchFieldInput = styled('input', {
+  width: '100%',
+  fontSize: '$label1',
+  fontWeight: '$semibold',
+  lineHeight: '$normal',
+  padding: '0',
+  border: 0,
+  borderRadius: '$md',
+  backgroundColor: 'transparent',
+  '&::placeholder': {
+    color: '#B4B4B4',
+    fontSize: '$label1',
+    fontWeight: '$semibold',
+  },
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+})
+
+export const StyledSearchFieldBox = styled('div', {
+  display: 'flex',
+  height: '$8',
+  padding: '0 $4',
+  gap: '$2',
+  fontFamily: '$sans',
+  flexDirection: 'row',
+  alignItems: 'center',
+  borderWidth: '$light',
+  borderStyle: 'solid',
+  borderColor: '$divider',
+  borderRadius: '$md',
+  color: '$typeHeading',
+  backgroundColor: '$whiteText',
+  transition: '$default',
+  minWidth: '$48',
+  outline: 'none',
+
+  [`${StyledButton}`]: {
+    transform: 'scale(0.9)',
+  },
+
+  '.cdg-searchfield-button': {
+    padding: '0',
+    width: '$6',
+    height: '$6',
+  },
+
+  '.cdg-searchfield-button .children svg': {
+    width: '$4',
+    height: '$4',
+  },
+
+  variants: {
+    disabled: {
+      true: {
+        color: '$disabledText',
+        backgroundColor: '$gray20',
+      },
+    },
+    isErrored: {
+      true: {
+        borderColor: '$danger',
+        borderWidth: '$medium',
+        '&:focus-within': {
+          borderColor: '$danger',
+        },
+      },
+    },
+    isDarkTheme: {
+      true: {
+        [`${StyledSearchFieldInput}`]: {
+          color: '$tertiaryText',
+          '&:focus': {
+            borderColor: '$cdgBlue120',
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+        '&:focus-within': {
+          borderColor: '$cdgBlue120',
+        },
+      },
+      false: {
+        [`${StyledSearchFieldInput}`]: {
+          '&:focus': {
+            borderColor: '$cdgBlue',
+            outline: 'none',
+            boxShadow: 'none',
+          },
+        },
+        '&:focus-within': {
+          borderColor: '$cdgBlue',
+        },
+      },
+    },
+    h5: {
+      true: {
+        flexDirection: 'row-reverse',
+        gap: '$2',
+        padding: '$2 $4',
+        borderRadius: '$lg',
+        height: '$12',
+        [`${StyledButton}`]: {
+          padding: 0,
+          color: '$grayShades60',
+        },
+        [`${StyledSearchFieldInput}`]: {
+          padding: 0,
+          '&::placeholder': {
+            fontSize: '$3_5',
+            fontStyle: 'normal',
+            fontWeight: '$medium',
+            lineHeight: '20px',
+          },
+        },
+      },
+      false: {
+        flexDirection: 'row',
+      },
+    },
+  },
+})
+
+export const StyledTextFieldHelperText = styled('div', {
+  fontSize: '$label2',
+  fontWeight: '$medium',
+  transition: '$default',
+  marginTop: '$1',
+  color: '$gray70',
+
+  variants: {
+    error: {
+      true: {
+        color: '$danger',
+      },
+    },
+  },
+})
+
+export const StyledSearchFieldBoxWrapper = styled('div', {
+  width: '100%',
+  display: 'block',
+  fontFamily: '$sans',
+})
+
+export const StyledTextFieldLabel = styled('label', {
+  fontSize: '$label1',
+  fontWeight: '$semibold',
+  marginBottom: '$2',
+  display: 'inline-block',
+  width: '100%',
+  '.asterisk': {
+    marginLeft: '$1',
+    color: '$danger',
+  },
+})
+
+export type SearchFieldVariantProps = VariantProps<
+  typeof StyledSearchFieldInput
+>
