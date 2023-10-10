65dccddb8a53de3650979d231a1e0e3d69bd6648
Kai Vu Duc Khai <142210010+kaiceresy@users.noreply.github.com>
2023-10-10 23:43:34 +0700

    [CDS-927] Multiple choices slider - use static CSS (#1244)

--- packages/static/src/index.ts
+++ packages/static/src/index.ts
@@ -4,6 +4,11 @@ export {default as Chip} from './chip'
 export type {ChipProps} from './chip'
 export {default as Icon} from './icon'
 export type {IconProp} from './icon'
+export {default as MultipleChoicesSlider} from './multiple-choices-slider'
+export type {
+  MultipleChoicesSliderItemProps,
+  MultipleChoicesSliderProps,
+} from './multiple-choices-slider'
 export {default as Popover} from './popover'
 export type {PopoverDirection, PopoverProps} from './popover'
 export {default as Spinner} from './spinner'
