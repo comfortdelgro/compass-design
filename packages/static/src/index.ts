a4de83424fe14c3d6d9ad1b2853d290cfb3d5d01
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-10-10 13:50:56 +0700

    [sidebar] change to static part 1

--- packages/static/src/index.ts
+++ packages/static/src/index.ts
@@ -6,7 +6,15 @@ export {default as Icon} from './icon'
 export type {IconProp} from './icon'
 export {default as Popover} from './popover'
 export type {PopoverDirection, PopoverProps} from './popover'
+export {default as Sidebar} from './sidebar'
+export type {
+  SidebarActionsProps,
+  SidebarContentProps,
+  SidebarProps,
+  SidebarTitleProps,
+} from './sidebar'
 export {default as Spinner} from './spinner'
 export type {SpinnerProps} from './spinner'
+
 export * from './theme'
 export {default as ThemeStaticProvider} from './theme/ThemeProvider'
