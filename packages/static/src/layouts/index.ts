343ba0366b3d5c7a2053422c6a47b7c83a3a6b25
fpt-duongtx <duongtx@fpt.com>
2023-10-10 14:43:06 +0700

    Add new action bar layout

---
+++ packages/static/src/layouts/index.ts
@@ -0,0 +1,7 @@
+export {default as ActionBar} from './action-bar'
+export type {
+  ActionBarCenterGroupProps,
+  ActionBarLeftGroupProps,
+  ActionBarProps,
+  ActionBarRightGroupProps,
+} from './action-bar'
