65d6cdaf04f8b30fd4f331cb16870ef8534e7c3e
michael-le-19 <minhlt19@fsoft.com.vn>
2023-10-16 08:59:04 +0700

    feature: range calendar static

--- packages/static/src/index.ts
+++ packages/static/src/index.ts
@@ -1,24 +1,24 @@
-export {default as Avatar} from './avatar'
-export type {AvatarProps} from './avatar'
-export {default as Breadcrumbs} from './breadcrumbs'
-export type {BreadcrumbItemProps, BreadcrumbsProps} from './breadcrumbs'
-export {default as Button} from './button'
-export type {ButtonProps} from './button'
-export {default as Calendar} from './calendar'
-export type {CalendarProps} from './calendar'
-export {default as Chart} from './chart'
+export { default as Avatar } from './avatar'
+export type { AvatarProps } from './avatar'
+export { default as Breadcrumbs } from './breadcrumbs'
+export type { BreadcrumbItemProps, BreadcrumbsProps } from './breadcrumbs'
+export { default as Button } from './button'
+export type { ButtonProps } from './button'
+export { default as Calendar } from './calendar'
+export type { CalendarProps } from './calendar'
+export { default as Chart } from './chart'
 export type {
   AreaChartProps,
   BarChartProps,
   ColumnChartProps,
   LineChartProps,
 } from './chart'
-export {default as Checkbox} from './checkbox'
-export type {CheckboxProps} from './checkbox'
-export {default as Chip} from './chip'
-export type {ChipProps} from './chip'
-export {default as Divider} from './divider'
-export type {DividerProps} from './divider'
+export { default as Checkbox } from './checkbox'
+export type { CheckboxProps } from './checkbox'
+export { default as Chip } from './chip'
+export type { ChipProps } from './chip'
+export { default as Divider } from './divider'
+export type { DividerProps } from './divider'
 // export {default as Drawer} from './drawer'
 // export type {
 //   DrawerDefaultProps,
@@ -27,42 +27,43 @@ export type {DividerProps} from './divider'
 //   DrawerHeaderProps,
 //   DrawerProps,
 // } from './drawer'
-export {default as Dropdown} from './dropdown'
+export { default as Dropdown } from './dropdown'
 export type {
   DropdownComboBoxProps,
   DropdownItemProps,
   DropdownProps,
   DropdownSelectProps,
 } from './dropdown'
-export {default as Icon} from './icon'
-export type {IconProp} from './icon'
-export {default as ImageViewer} from './image-viewer'
-export type {ImageViewerProps} from './image-viewer'
+export { default as Icon } from './icon'
+export type { IconProp } from './icon'
+export { default as ImageViewer } from './image-viewer'
+export type { ImageViewerProps } from './image-viewer'
 export * from './layouts'
 // export {default as MultipleChoicesSlider} from './multiple-choices-slider'
 // export type {
 //   MultipleChoicesSliderItemProps,
 //   MultipleChoicesSliderProps,
 // } from './multiple-choices-slider'
-export {default as MultipleDropdown} from './multiple-dropdown'
+export { default as MultipleDropdown } from './multiple-dropdown'
 export type {
   MultipleDropdownItemProps,
   MultipleDropdownProps,
 } from './multiple-dropdown'
-
-export {default as Popover} from './popover'
-export type {PopoverDirection, PopoverProps} from './popover'
-export {default as Progress} from './progress'
-export type {CircularProgressProps, LinearProgressProps} from './progress'
-export {default as Radio} from './radio'
-export type {RadioGroupProps, RadioProps} from './radio'
-export {default as Rating} from './rating'
-export type {RatingProps} from './rating'
-export {default as SearchField} from './searchfield'
-export type {SearchFieldProps} from './searchfield'
-export {default as Spinner} from './spinner'
-export type {SpinnerProps} from './spinner'
-export {default as Status} from './status'
-export type {StatusProps} from './status'
+export { default as Popover } from './popover'
+export type { PopoverDirection, PopoverProps } from './popover'
+export { default as Progress } from './progress'
+export type { CircularProgressProps, LinearProgressProps } from './progress'
+export { default as Radio } from './radio'
+export type { RadioGroupProps, RadioProps } from './radio'
+export { default as Rating } from './rating'
+export type { RatingProps } from './rating'
+export { default as RangeCalendar } from './range-calendar'
+export type { RangeCalendarProps, RangeCalendarShorcutItem, RangeValue } from './range-calendar'
+export { default as SearchField } from './searchfield'
+export type { SearchFieldProps } from './searchfield'
+export { default as Spinner } from './spinner'
+export type { SpinnerProps } from './spinner'
+export { default as Status } from './status'
+export type { StatusProps } from './status'
 export * from './theme'
-export {default as ThemeStaticProvider} from './theme/ThemeProvider'
+export { default as ThemeStaticProvider } from './theme/ThemeProvider'
---
+++ packages/static/src/range-calendar/RangeCalendar.stories.tsx
@@ -0,0 +1,101 @@
+import { Meta } from "@storybook/react"
+import React from "react"
+import RangeCalendar, { RangeCalendarShorcutItem, RangeValue } from "."
+import { Button, CalendarProps } from ".."
+import { DateValue, endOfWeek, getLocalTimeZone, parseDate, startOfWeek, today } from "../internationalized/date"
+import { useDateFormatter, useLocale } from "../internationalized/i18n"
+
+export const Basic = () => {
+  const [range, setRange] = React.useState<RangeValue<CalendarProps | null>>({
+    start: parseDate('2020-02-03'),
+    end: parseDate('2020-02-08'),
+  })
+  const formatter = useDateFormatter({ dateStyle: 'long' })
+  return (
+    <div style={{ padding: '1rem', backgroundColor: 'var(--cdg-color-gray20)' }}>
+      {range.start && range.end
+        ? formatter.formatRange(
+          range.start.toDate(getLocalTimeZone()),
+          range.end.toDate(getLocalTimeZone()),
+        )
+        : `${range.start &&
+        formatter.format(range.start.toDate(getLocalTimeZone()))
+        } - ${range.end && formatter.format(range.end.toDate(getLocalTimeZone()))
+        }`}
+      { }
+
+      <RangeCalendar
+        value={range}
+        onChange={(e) => {
+          setRange(e as RangeValue<DateValue>)
+        }}
+        hasFooter
+      />
+    </div>
+  )
+}
+
+export const Variants: React.FC = () => {
+  const { locale } = useLocale()
+
+  const dayOfTwoWeeksAgo = today(getLocalTimeZone()).subtract({
+    weeks: 2,
+  })
+  const startOfTwoWeeksAgo = startOfWeek(dayOfTwoWeeksAgo, locale)
+  const endOfTwoWeeksAgo = endOfWeek(dayOfTwoWeeksAgo, locale)
+
+  return (
+    <div style={{ padding: '1rem', backgroundColor: 'var(--cdg-color-gray20)' }}>
+      <h3>Footer</h3>
+      <RangeCalendar hasFooter />
+      <h3>Footer & Shorcuts</h3>
+      <RangeCalendar
+        hasFooter
+        hasShortcuts
+        onSearchButtonClick={() => {
+          alert('search button callback')
+        }}
+      />
+      <h3>Custom Footer & Custom Shorcuts</h3>
+      <RangeCalendar
+        hasFooter
+        hasShortcuts
+        customShortcuts={(defaultShortcuts, isInvalid) => {
+          const customShortcuts: RangeCalendarShorcutItem[] = [
+            {
+              label: 'Custom Shortcut (Two weeks ago)',
+              isDisable:
+                isInvalid(startOfTwoWeeksAgo) || isInvalid(endOfTwoWeeksAgo),
+              getValue: () => {
+                return { start: startOfTwoWeeksAgo, end: endOfTwoWeeksAgo }
+              },
+            },
+          ]
+          return [...defaultShortcuts, ...customShortcuts]
+        }}
+        ctaButtonRender={
+          <Button
+            onPress={() => {
+              alert('Custom callback')
+            }}
+          >
+            Custom Button
+          </Button>
+        }
+      />
+    </div>
+  )
+}
+
+const meta = {
+  title: 'Example/Range Calendar Server',
+  component: Basic,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Basic>
+
+export default meta
