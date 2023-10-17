ca7ecd9e6abf84d2d351b8fd3886a80d8584f2f7
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-10-17 16:05:09 +0700

    feat: CDS-897

--- packages/static/src/file/preview.tsx
+++ packages/static/src/file/preview.tsx
@@ -36,12 +36,13 @@ const FilePreview = React.forwardRef<HTMLDivElement, FilePreviewProps>(
     return (
       <CssInjection css={css} childrenRef={previewRef}>
         <div ref={previewRef} className={previewClasses} {...htmlProps}>
-          <div className={`cdg-file-preview-image ${styles.imagePreview}`}>
+          <div className={`cdg-file-preview-image-wrapper ${styles.imagePreviewWrapper}`}>
             {imageSrc && !failed ? (
               <img
-                src={imageSrc}
                 alt='preview'
+                src={imageSrc}
                 onError={() => setFailed(true)}
+                className={`cdg-file-preview-image ${styles.previewImage}`}
               />
             ) : imageSrc && failed ? (
               <Fallback />
