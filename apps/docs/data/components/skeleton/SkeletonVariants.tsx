536087cebe3d20d10eed8aecc6ac2eb36fe3251f
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-10-23 11:23:20 +0700

    feature: CDS-931

--- apps/docs/data/components/skeleton/SkeletonVariants.tsx
+++ apps/docs/data/components/skeleton/SkeletonVariants.tsx
@@ -1,4 +1,5 @@
 import {Skeleton} from '@comfortdelgro/react-compass'
+// import {Skeleton as StaticSkeleton} from '@comfortdelgro/static'
 
 function SkeletonVariants() {
   return (
@@ -28,6 +29,29 @@ function SkeletonVariants() {
       <Skeleton variant='rectangular' color='var(--colors-gray40)'>
         <div style={{width: 210, height: 120}} />
       </Skeleton>
+
+      {/* <StaticSkeleton variant='text' width={210} color='var(--colors-gray40)' />
+      <StaticSkeleton
+        variant='circular'
+        width={40}
+        height={40}
+        color='var(--colors-gray40)'
+      />
+      <StaticSkeleton
+        variant='rectangular'
+        width={210}
+        height={60}
+        color='var(--colors-gray40)'
+      />
+      <StaticSkeleton
+        variant='rounded'
+        width={210}
+        height={60}
+        color='var(--colors-gray40)'
+      />
+      <StaticSkeleton variant='rectangular' color='var(--colors-gray40)'>
+        <div style={{width: 210, height: 120}} />
+      </StaticSkeleton> */}
     </div>
   )
 }
