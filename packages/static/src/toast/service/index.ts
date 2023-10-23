70683696c3f75b9c5742d7d07d03f9355540ab93
TuDQ <quoctu2014@gmail.com>
2023-10-24 00:19:05 +0700

    convert Toast component to static css

---
+++ packages/static/src/toast/service/index.ts
@@ -0,0 +1,7 @@
+import {useToast} from '../hooks/useToast'
+import {ToastContextProvider} from './toast-context'
+
+export type {ToastsContainerProps} from './toast-container'
+export type {ToastContextValue} from './toast-context'
+export * from './types'
+export {ToastContextProvider, useToast}
