72cc8581679fc36b95ba9815709f3b1b7070667e
FSOFT.FPT.VN\TuDQ3 <tudq3@fpt.com>
2023-10-24 23:07:00 +0700

    Convert TagBoxV2 to static css

--- packages/static/src/index.ts
+++ packages/static/src/index.ts
@@ -238,6 +238,8 @@ export type {
   SubBannerProps,
   SubBannerTitleProps,
 } from './subBanner'
+export {default as TagBoxV2} from './tag-box-v2'
+export type {Tag, TagBoxV2Props} from './tag-box-v2'
 export {default as Textarea} from './textarea'
 export type {TextareaProps} from './textarea'
 export {default as TextField} from './textfield'
