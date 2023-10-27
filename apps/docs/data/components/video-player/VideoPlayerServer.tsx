d4a0caed6f62ee8d91cd23dc44065ea804ed3d2d
Nguyen Duy Hien <nguyenduy.hien@petronas.com>
2023-10-27 16:33:37 +0700

    [video player] test server

---
+++ apps/docs/data/components/video-player/VideoPlayerServer.tsx
@@ -0,0 +1,9 @@
+import {VideoPlayer} from '@comfortdelgro/static'
+
+function BasicExample() {
+  return (
+    <VideoPlayer src='http://techslides.com/demos/sample-videos/small.webm' />
+  )
+}
+
+export default BasicExample
