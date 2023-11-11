435e8eaea80085b8f82e720287ea37d67027562f
yagin94 <50587656+yagin94@users.noreply.github.com>
2023-11-12 02:25:29 +0700

    [CDS-988] add utils components (#1385)
    * [component.ts] change styled to components
    
    * [change] change some structure file
    
    * [change] change import
    
    * [component.module.css] remove cmt
    
    ---------
    
    Co-authored-by: Yagin <hiennd14@fpt.com>

---
+++ packages/static/src/utils/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Flexbox } from './Flexbox';
+export type { FlexboxComponentProps } from './Flexbox';
+export { default as Row } from './Row';
+export type { RowComponentProps } from './Row';
+export { default as Column } from './Column';
+export type { ColumnComponentProps } from './Column';
