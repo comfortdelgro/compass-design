19fdce6a95676e8a8532309b801df9ab6d72a6c7
Yagin <hiennd14@fpt.com>
2023-11-12 02:04:00 +0700

    [change] change some structure file

--- packages/static/src/utils/components.tsx
+++
@@ -1,6 +0,0 @@
-export { default as Flexbox } from './components/Flexbox';
-export type { FlexboxComponentProps } from './components/Flexbox';
-export { default as Row } from './components/Row';
-export type { RowComponentProps } from './components/Row';
-export { default as Column } from './components/Column';
-export type { ColumnComponentProps } from './components/Column';
