d3062191a192aeecbb5ab781eb4769a30986fbf0
Tai Bui <tai.c.bui@gmail.com>
2023-11-12 21:37:35 +0700

    fix build error

--- packages/static/src/transitions/transitions.stories.tsx
+++ packages/static/src/transitions/transitions.stories.tsx
@@ -2,7 +2,7 @@ import type {Meta} from '@storybook/react'
 import React from 'react'
 
 // replace this ass static toggle asap
-import Toggle from '../../../react-compass/src/toggle'
+import Toggle from './../toggle'
 import Transitions from './index'
 
 export const Variants: React.FC = () => {
