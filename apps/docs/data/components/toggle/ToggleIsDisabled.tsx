bb7f28ff4a059d3d389f2c8cbf16b2c186676463
fpt-duongtx <duongtx@fpt.com>
2023-11-21 13:48:32 +0700

    Fix for static components batch 2

--- apps/docs/data/components/toggle/ToggleIsDisabled.tsx
+++ apps/docs/data/components/toggle/ToggleIsDisabled.tsx
@@ -2,8 +2,9 @@ import Toggle from '@comfortdelgro/react-compass-old/toggle'
 
 function ToggleIsDisabled() {
   return (
-    <div>
+    <div style={{display: 'flex', gap: '12px'}}>
       <Toggle size='lg' isDisabled />
+      <Toggle size='lg' isSelected={true} isDisabled />
     </div>
   )
 }
