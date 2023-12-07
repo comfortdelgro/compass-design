a098c2dd3796790da91859f56cd41eb3a8305ae3
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-12-07 15:15:40 +0700

    feat: CDS-1014

--- packages/static/src/pagination/index.ts
+++ packages/static/src/pagination/index.ts
@@ -1,7 +1,10 @@
+import ItemCounting from './components/ItemCounting'
 import Pagination from './pagination'
 
 export type {PaginationProps} from './pagination'
 
 Pagination.displayName = 'Pagination'
 
-export default Pagination
+export default Pagination as typeof Pagination & {
+  ItemsCounting: typeof ItemCounting
+}
