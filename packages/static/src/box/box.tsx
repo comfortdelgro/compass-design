0240867c82982da7c8a040487165bff3a54a4680
Tee Nguyen <tungnt107@fsoft.com.vn>
2023-12-11 11:14:07 +0700

    fix: CDS-1016

--- packages/static/src/box/box.tsx
+++ packages/static/src/box/box.tsx
@@ -12,7 +12,6 @@ interface Props {
   css?: unknown
   className?: string
   as?: React.ElementType
-  className?: string
   border?: string
   borderBottom?: string
   borderColor?: string
@@ -102,7 +101,6 @@ const Box = React.forwardRef<HTMLElement, BoxProps>((props, ref) => {
     children,
     // styled component props
     css = {},
-    className = '',
     // custom props
     as: asProp = 'div',
     border,
