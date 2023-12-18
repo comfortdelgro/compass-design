f99abafef0a090008b7ee63f7f6e07c9b87a6853
Yagin <hiennd14@fpt.com>
2023-12-18 10:16:26 +0700

    [table][fix width of table progress for resizing]

--- packages/static/src/table/table.stories.tsx
+++ packages/static/src/table/table.stories.tsx
@@ -8,15 +8,14 @@ import {
   faPencil,
   faTrashAlt,
 } from '@fortawesome/free-solid-svg-icons'
-import { Meta } from '@storybook/react'
-import React, { Key, MouseEvent, TouchEvent, useState } from 'react'
+import {Meta} from '@storybook/react'
+import React, {Key, MouseEvent, TouchEvent, useState} from 'react'
 import Table, {
   TableCellContext,
   TableColumnDef,
   TableHeaderContext,
   TableSortingState,
   useEditableCellContext,
-  TableColumnFiltersState,
 } from '.'
 import {
   Button,
@@ -35,7 +34,7 @@ import {
   makeData,
   makeRequestStatusData,
 } from './makeData'
-import { OptionType } from './table'
+import {OptionType} from './table'
 import ProgressPercentage from './table-progress'
 export const Basic: React.FC = () => {
   const [page, setPage] = useState(1)
@@ -45,7 +44,6 @@ export const Basic: React.FC = () => {
     enableMultiSort: true,
     columnResizeMode: 'onChange',
     manualSorting: false,
-    manualFiltering: true,
     enableRowSelection: (row) => row.original.age > 30,
   }
 
@@ -57,15 +55,12 @@ export const Basic: React.FC = () => {
   const onChangeRowSelection = (rowSelection: unknown) => {
     console.log(rowSelection)
   }
-  const onFiltering = (filtering: TableColumnFiltersState) => {
-    console.log(filtering)
-  }
 
   const columns = React.useMemo<Array<TableColumnDef<Person>>>(
     () => [
       {
         id: 'select',
-        header: ({ table }) => {
+        header: ({table}) => {
           return (
             <div
               style={{
@@ -82,7 +77,7 @@ export const Basic: React.FC = () => {
             </div>
           )
         },
-        cell: ({ row }) => (
+        cell: ({row}) => (
           <div
             style={{
               display: 'flex',
@@ -101,7 +96,7 @@ export const Basic: React.FC = () => {
       },
       {
         id: 'name',
-        header: () => <div style={{ textAlign: 'center' }}>Name</div>,
+        header: () => <div style={{textAlign: 'center'}}>Name</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
@@ -145,7 +140,7 @@ export const Basic: React.FC = () => {
       },
       {
         id: 'otherInfo',
-        header: () => <div style={{ textAlign: 'center' }}>Other info</div>,
+        header: () => <div style={{textAlign: 'center'}}>Other info</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
@@ -189,7 +184,6 @@ export const Basic: React.FC = () => {
       columns={columns}
       options={options}
       onManualSorting={onSorting}
-      onManualFilter={onFiltering}
       onChangeRowSelection={onChangeRowSelection}
     >
       <Table.Toolbar
@@ -271,7 +265,7 @@ export const ExpandableRow: React.FC = () => {
       {
         id: 'expander',
         size: 40,
-        header: ({ table }: TableHeaderContext<LimitRequestStatus, unknown>) => (
+        header: ({table}: TableHeaderContext<LimitRequestStatus, unknown>) => (
           <span
             onClick={table.getToggleAllRowsExpandedHandler()}
             style={{
@@ -289,7 +283,7 @@ export const ExpandableRow: React.FC = () => {
             )}
           </span>
         ),
-        cell: ({ row }: TableCellContext<LimitRequestStatus, unknown>) => (
+        cell: ({row}: TableCellContext<LimitRequestStatus, unknown>) => (
           <span
             onClick={() => row.toggleExpanded()}
             style={{
@@ -311,7 +305,7 @@ export const ExpandableRow: React.FC = () => {
       {
         id: 'accountName',
         accessorKey: 'accountName',
-        header: () => <div style={{ textAlign: 'center' }}>Account Name</div>,
+        header: () => <div style={{textAlign: 'center'}}>Account Name</div>,
         footer: (props: TableHeaderContext<LimitRequestStatus, unknown>) =>
           props.column.id,
       },
@@ -319,7 +313,7 @@ export const ExpandableRow: React.FC = () => {
         id: 'code',
         accessorKey: 'code',
 
-        header: () => <div style={{ textAlign: 'center' }}>Code</div>,
+        header: () => <div style={{textAlign: 'center'}}>Code</div>,
         footer: (props: TableHeaderContext<LimitRequestStatus, unknown>) =>
           props.column.id,
       },
@@ -328,7 +322,7 @@ export const ExpandableRow: React.FC = () => {
         accessorKey: 'requestLimit',
 
         header: () => (
-          <div style={{ textAlign: 'center' }}>New Request Limit</div>
+          <div style={{textAlign: 'center'}}>New Request Limit</div>
         ),
         footer: (props: TableHeaderContext<LimitRequestStatus, unknown>) =>
           props.column.id,
@@ -338,7 +332,7 @@ export const ExpandableRow: React.FC = () => {
         size: 280,
 
         accessorKey: 'status',
-        header: () => <div style={{ textAlign: 'center' }}>Status</div>,
+        header: () => <div style={{textAlign: 'center'}}>Status</div>,
         footer: (props: TableHeaderContext<LimitRequestStatus, unknown>) =>
           props.column.id,
       },
@@ -362,8 +356,8 @@ export const ExpandableRow: React.FC = () => {
         {rowData.progress?.map((processItem) => {
           return (
             <>
-              <p style={{ fontSize: '12px' }}>{processItem.remarks}</p>
-              <p style={{ fontSize: '11px', fontWeight: 200, color: '#878787' }}>
+              <p style={{fontSize: '12px'}}>{processItem.remarks}</p>
+              <p style={{fontSize: '11px', fontWeight: 200, color: '#878787'}}>
                 {processItem.createdAt.toDateString()}
               </p>
               {/* <Divider /> */}
@@ -395,8 +389,8 @@ export const ExpandableRow: React.FC = () => {
             justifyContent: 'space-between',
           }}
         >
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <span style={{ width: 250 }}>Rows per page: </span>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <span style={{width: 250}}>Rows per page: </span>
             <Dropdown.Select defaultSelectedKey='10'>
               <Dropdown.Item key='10'>10 rows</Dropdown.Item>
               <Dropdown.Item key='20'>20 rows</Dropdown.Item>
@@ -439,8 +433,8 @@ export const EmptyState: React.FC = () => {
     columnResizeMode: 'onChange',
     manualSorting: false,
     initialSortBy: [
-      { id: 'firstName', desc: true },
-      { id: 'lastName', desc: false },
+      {id: 'firstName', desc: true},
+      {id: 'lastName', desc: false},
     ],
   }
 
@@ -448,7 +442,7 @@ export const EmptyState: React.FC = () => {
     () => [
       {
         id: 'select',
-        header: ({ table }) => {
+        header: ({table}) => {
           return (
             <div
               style={{
@@ -468,7 +462,7 @@ export const EmptyState: React.FC = () => {
           )
         },
         enableGrouping: false,
-        cell: ({ row }) => (
+        cell: ({row}) => (
           <div
             style={{
               display: 'flex',
@@ -489,7 +483,7 @@ export const EmptyState: React.FC = () => {
       },
       {
         id: 'name',
-        header: () => <div style={{ textAlign: 'center' }}>Name</div>,
+        header: () => <div style={{textAlign: 'center'}}>Name</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
@@ -513,7 +507,7 @@ export const EmptyState: React.FC = () => {
       },
       {
         id: 'otherInfo',
-        header: () => <div style={{ textAlign: 'center' }}>Other info</div>,
+        header: () => <div style={{textAlign: 'center'}}>Other info</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
@@ -630,8 +624,8 @@ export const Loading: React.FC = () => {
     columnResizeMode: 'onChange',
     manualSorting: false,
     initialSortBy: [
-      { id: 'firstName', desc: true },
-      { id: 'lastName', desc: false },
+      {id: 'firstName', desc: true},
+      {id: 'lastName', desc: false},
     ],
   }
 
@@ -639,7 +633,7 @@ export const Loading: React.FC = () => {
     () => [
       {
         id: 'select',
-        header: ({ table }) => {
+        header: ({table}) => {
           return (
             <div
               style={{
@@ -659,7 +653,7 @@ export const Loading: React.FC = () => {
           )
         },
         enableGrouping: false,
-        cell: ({ row }) => (
+        cell: ({row}) => (
           <div
             style={{
               display: 'flex',
@@ -680,7 +674,7 @@ export const Loading: React.FC = () => {
       },
       {
         id: 'name',
-        header: () => <div style={{ textAlign: 'center' }}>Name</div>,
+        header: () => <div style={{textAlign: 'center'}}>Name</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
@@ -704,7 +698,7 @@ export const Loading: React.FC = () => {
       },
       {
         id: 'otherInfo',
-        header: () => <div style={{ textAlign: 'center' }}>Other info</div>,
+        header: () => <div style={{textAlign: 'center'}}>Other info</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
@@ -833,13 +827,17 @@ export const EditableTemplateCell: React.FC = () => {
     enableMultiSort: true,
     columnResizeMode: 'onChange',
     manualSorting: false,
+    initialSortBy: [
+      {id: 'firstName', desc: true},
+      {id: 'lastName', desc: false},
+    ],
     enableRowSelection: (row) => row.original.age > 30,
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const onSorting = (sortingField: TableSortingState) => { }
+  const onSorting = (sortingField: TableSortingState) => {}
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const onChangeRowSelection = (rowSelection: any) => { }
+  const onChangeRowSelection = (rowSelection: any) => {}
 
   const columns = React.useMemo<Array<TableColumnDef<Person>>>(
     () => [
@@ -1038,7 +1036,7 @@ const PersonTemplateCell = () => {
         md={2}
         lg={2}
         xl={2}
-        css={{ display: 'flex', justifyContent: 'center', gap: 5 }}
+        css={{display: 'flex', justifyContent: 'center', gap: 5}}
       >
         <Button onClick={handleConfirm} size='sm'>
           <Icon icon={faCheck} />
@@ -1083,8 +1081,8 @@ export const DataGrid: React.FC = () => {
     columnResizeMode: 'onChange',
     manualSorting: false,
     initialSortBy: [
-      { id: 'firstName', desc: true },
-      { id: 'lastName', desc: false },
+      {id: 'firstName', desc: true},
+      {id: 'lastName', desc: false},
     ],
   }
 
@@ -1099,7 +1097,7 @@ export const DataGrid: React.FC = () => {
     () => [
       {
         id: 'select',
-        header: ({ table }) => {
+        header: ({table}) => {
           return (
             <div
               style={{
@@ -1119,7 +1117,7 @@ export const DataGrid: React.FC = () => {
           )
         },
         enableGrouping: false,
-        cell: ({ row }) => (
+        cell: ({row}) => (
           <div
             style={{
               display: 'flex',
@@ -1140,7 +1138,7 @@ export const DataGrid: React.FC = () => {
       },
       {
         id: 'name',
-        header: () => <div style={{ textAlign: 'center' }}>Name</div>,
+        header: () => <div style={{textAlign: 'center'}}>Name</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
@@ -1164,7 +1162,7 @@ export const DataGrid: React.FC = () => {
       },
       {
         id: 'otherInfo',
-        header: () => <div style={{ textAlign: 'center' }}>Other info</div>,
+        header: () => <div style={{textAlign: 'center'}}>Other info</div>,
         footer: (props) => props.column.id,
         enableGrouping: false,
         columns: [
