b307e8f22028f4669030c3de0c1221844076f471
Tai Bui <tai.c.bui@gmail.com>
2023-12-24 18:00:37 +0700

    Update

---
+++ packages/static/src/timeline/timeline-item.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {StyledComponentProps} from '../utils/stitches.types'
+import {useDOMRef} from '../utils/use-dom-ref'
+import {
+  StyledItemContainer,
+  StyledItemContentWrapper,
+  StyledItemDot,
+  StyledItemHeaderContainer,
+  StyledItemLabel,
+} from './styles/timeline.styles'
+
+interface Props extends StyledComponentProps {
+  children?: React.ReactNode
+  label?: React.ReactNode | string
+  icon?: React.ReactNode
+  dot?: React.ReactNode
+  identifier?: number
+}
+
+export type TimelineItemProps = Props &
+  Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
+
+const TimelineItem = React.forwardRef<HTMLDivElement, TimelineItemProps>(
+  (props, ref) => {
+    const {
+      children,
+      css = {},
+      label = '',
+      identifier = 0,
+      icon,
+      dot,
+      ...delegated
+    } = props
+    const timelineItemRef = useDOMRef<HTMLDivElement>(ref)
+
+    // classname for styling alternate
+    const className = (prefix: string) => {
+      if (identifier % 2 === 0) {
+        return `${prefix}-even`
+      }
+      return `${prefix}-odd`
+    }
+    return (
+      <>
+        <StyledItemContainer
+          css={css}
+          ref={timelineItemRef}
+          {...delegated}
+          className={className('itemContainer')}
+        >
+          <StyledItemHeaderContainer className={className('header')}>
+            {!dot ? <StyledItemDot>{icon}</StyledItemDot> : dot}
+            {typeof label === 'string' ? (
+              <StyledItemLabel>{label}</StyledItemLabel>
+            ) : (
+              label
+            )}
+          </StyledItemHeaderContainer>
+          <StyledItemContentWrapper className={className('content')}>
+            {children}
+          </StyledItemContentWrapper>
+        </StyledItemContainer>
+      </>
+    )
+  },
+)
+
+export default TimelineItem
---
+++ packages/static/src/timeline/timeline.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import {StyledComponentProps} from '../utils/stitches.types'
+import {useDOMRef} from '../utils/use-dom-ref'
+import {
+  StyledTimeline,
+  StyledTimeLineContainer,
+  TimelineVariantProps,
+} from './styles/timeline.styles'
+import TimelineItem from './timeline-item'
+
+interface Props extends StyledComponentProps {
+  children?: React.ReactNode
+  isDisabled?: boolean
+}
+
+export type TimelineProps = Props &
+  TimelineVariantProps &
+  Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
+
+const Timeline = React.forwardRef<HTMLDivElement, TimelineProps>(
+  (props, ref) => {
+    const {
+      children,
+      // StyledComponentProps
+      css = {},
+      // VariantProps
+      mode = 'vertical',
+      labelAlignment = 'right',
+      itemAlignment = 'right',
+
+      // HTMLDiv Props
+      ...delegated
+    } = props
+
+    const timelineRef = useDOMRef<HTMLDivElement>(ref)
+
+    return (
+      <StyledTimeLineContainer
+        className='cdg-timeline-container'
+        mode={mode}
+        labelAlignment={labelAlignment}
+        itemAlignment={itemAlignment}
+      >
+        <StyledTimeline
+          css={css}
+          ref={timelineRef}
+          {...delegated}
+          mode={mode}
+          labelAlignment={labelAlignment}
+          itemAlignment={itemAlignment}
+          className='cdg-timeline'
+        >
+          {children}
+        </StyledTimeline>
+      </StyledTimeLineContainer>
+    )
+  },
+)
+
+export default Timeline as typeof Timeline & {
+  Item: typeof TimelineItem
+}
