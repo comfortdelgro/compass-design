c27900d79753192e89efac99f5de269969f48418
Tee Nguyen Thanh Tung <tungnt107@fsoft.com.vn>
2024-02-16 18:59:34 +0700

    fix: CDS-1072

--- apps/docs/data/components/dashboard-sidecard/DashboardSidecard.tsx
+++ apps/docs/data/components/dashboard-sidecard/DashboardSidecard.tsx
@@ -1,133 +1,33 @@
 import {
-  Avatar,
-  Button,
+  Card,
   DashboardSidecard,
 } from '@comfortdelgro/react-compass'
+
+const lorem = ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste
+  corrupti esse qui earum dolorum nulla assumenda excepturi, a
+  quibusdam rem eligendi vero hic, doloremque nam deserunt,
+  consequuntur aut atque eveniet.
+  `
 
 const Default: React.FC = () => {
   return (
     <DashboardSidecard>
       <DashboardSidecard.Header>My header</DashboardSidecard.Header>
       <DashboardSidecard.Content>
-        <div
-          style={{
-            backgroundColor: 'white',
-            borderRadius: '8px',
-            width: '100%',
-            height: '11rem',
-            padding: '1rem',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div style={{fontWeight: '500', fontSize: '1.2em'}}>Title</div>
-          <div
-            style={{
-              fontSize: '0.8em',
-              textAlign: 'justify',
-            }}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo hic
-            quae, adipisci tempore minus beatae itaque tenetur.
-          </div>
-          <Avatar.Group display={3} size='sm'>
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a042581f4e29026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a0581f4e29d026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a042581f4e429026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a042581f4e42239026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a0425e81f4234e429026704d'
-              size='sm'
-            />
-          </Avatar.Group>
-        </div>
+        <Card size='lg'>
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <p style={{margin: 0}}>{lorem.slice(0, lorem.length / 2)}</p>
+          </Card.Body>
+        </Card>
       </DashboardSidecard.Content>
       <DashboardSidecard.Content>
-        <div
-          style={{
-            backgroundColor: 'white',
-            borderRadius: '8px',
-            width: '100%',
-            height: '11rem',
-            padding: '1rem',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          }}
-        >
-          <div style={{fontWeight: '500', fontSize: '1.2em'}}>Title</div>
-          <div
-            style={{
-              fontSize: '0.8em',
-              textAlign: 'justify',
-            }}
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo hic
-            quae, adipisci tempore minus beatae itaque tenetur.
-          </div>
-          <Avatar.Group display={3} size='sm'>
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a042581f4e29026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a0581f4e29d026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a042581f4e429026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a042581f4e42239026704d'
-              size='sm'
-            />
-            <Avatar
-              label='Albert Einstein'
-              image='https://i.pravatar.cc/150?u=a0425e81f4234e429026704d'
-              size='sm'
-            />
-          </Avatar.Group>
-        </div>
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Button
-            className='c-hKssGC-hYGuEl-variant-primary'
-            css={{width: '48%'}}
-          >
-            Action
-          </Button>
-          <Button variant='danger' css={{width: '48%'}}>
-            Warning
-          </Button>
-        </div>
+        <Card size='lg'>
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <p style={{margin: 0}}>{lorem.slice(0, lorem.length / 2)}</p>
+          </Card.Body>
+        </Card>
       </DashboardSidecard.Content>
     </DashboardSidecard>
   )
