2eb5a4ffb475e978eb41bca2540d37ff936a2c2f
Tee Nguyen Thanh Tung <tungnt107@fsoft.com.vn>
2024-04-23 14:00:35 +0700

    fix

--- packages/react-compass/src/table/table-progress.tsx
+++ packages/react-compass/src/table/table-progress.tsx
@@ -1,48 +1,68 @@
 import React from 'react'
-import {CSS} from '../utils'
-import CssInjection from '../utils/objectToCss/CssInjection'
+import {CSS, CssInjection} from '../utils/objectToCss'
+import {useDOMRef} from '../utils/use-dom-ref'
 import styles from './styles/table-progress.module.css'
 
 interface Props {
-  progress: number
   css?: CSS
-  className?: string
+  progress: number
 }
 
-export type ProgressPercentageProps = Props &
+export type TableProgressProps = Props &
   Omit<React.HTMLAttributes<HTMLDivElement>, keyof Props>
 
-const ProgressPercentage: React.FC<ProgressPercentageProps> = ({
-  progress,
-  css = {},
-  className,
-}: ProgressPercentageProps) => {
-  let status = ''
-  if (progress >= 0 && progress <= 30) {
-    status = 'low'
-  } else if (progress >= 30 && progress <= 60) {
-    status = 'average'
-  } else {
-    status = 'high'
-  }
-  const ref = React.useRef()
-  const progressClass = [
-    styles.cdgTableProgress,
-    status === 'low' && styles.progressLow,
-    status === 'average' && styles.progressAverage,
-    status === 'high' && styles.progressHigh,
-    className,
-  ]
-    .filter(Boolean)
-    .join(' ')
-  return (
-    <CssInjection css={css} childrenRef={ref}>
-      <div className={styles.cdgTableProgressBar}>
-        <div className={progressClass} style={{width: `${progress}%`}}></div>
-        <span className={styles.cdgTableProgressLabel}>{`${progress}%`}</span>
-      </div>
-    </CssInjection>
-  )
-}
+const TableProgress = React.forwardRef<HTMLDivElement, TableProgressProps>(
+  (props, ref) => {
+    const {css = {}, progress, className} = props
+    const tableProgressRef = useDOMRef<HTMLDivElement>(ref)
 
-export default ProgressPercentage
+    const status = React.useMemo(() => {
+      if (progress >= 0 && progress <= 30) {
+        return 'low'
+      } else if (progress >= 30 && progress <= 60) {
+        return 'average'
+      } else {
+        return 'high'
+      }
+    }, [progress])
+
+    const rootClasses = [
+      styles.cdgTableProgressBar,
+      className,
+      'cdg-table-progress',
+    ]
+      .filter(Boolean)
+      .join(' ')
+
+    const progressClasses = [
+      styles.cdgTableProgress,
+      status === 'low' && styles.progressLow,
+      status === 'average' && styles.progressAverage,
+      status === 'high' && styles.progressHigh,
+      'cdg-table-progress-current',
+    ]
+      .filter(Boolean)
+      .join(' ')
+
+    const labelClasses = [
+      styles.cdgTableProgressLabel,
+      'cdg-table-progress-label',
+    ]
+      .filter(Boolean)
+      .join(' ')
+
+    return (
+      <CssInjection css={css} childrenRef={tableProgressRef}>
+        <div className={rootClasses} ref={tableProgressRef}>
+          <div
+            className={progressClasses}
+            style={{width: `${progress}%`}}
+          ></div>
+          <span className={labelClasses}>{`${progress}%`}</span>
+        </div>
+      </CssInjection>
+    )
+  },
+)
+
+export default TableProgress
