9d177d69045ebb9d41ab381c053469b7d623458b
Tee Nguyen Thanh Tung <tungnt107@fsoft.com.vn>
2024-06-11 11:12:13 +0700

    Merge branch 'develop' of https://github.com/comfortdelgro/compass-design into merge/develop

--- packages/react-compass/src/calendar/index.ts
+++ packages/react-compass/src/calendar/index.ts
@@ -1,13 +1,8 @@
 import * as InternationalizedDate from '../internationalized/date'
 import * as i18n from '../internationalized/i18n'
-import _Calendar from './calendar'
+import Calendar from './calendar'
 
 export type {CalendarProps} from './calendar'
-
-const Calendar = _Calendar as typeof _Calendar & {
-  InternationalizedDate: typeof InternationalizedDate
-  I18N: typeof i18n
-}
 
 Calendar.displayName = 'Calendar'
 Calendar.InternationalizedDate = InternationalizedDate
