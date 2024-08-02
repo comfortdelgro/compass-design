6baf0c697f5532cf99dd40b11274b34e61d45063
michael-le-19 <minhlt19@fsoft.com.vn>
2024-08-02 11:05:43 +0700

    feature: update document for date picker

---
+++ apps/docs/data/components/date-picker/MonthPicker.tsx
@@ -0,0 +1,11 @@
+import {DatePicker} from '@comfortdelgro/react-compass'
+
+function MonthPicker() {
+  return (
+    <div>
+      <DatePicker label='Month' picker='month' />
+    </div>
+  )
+}
+
+export default MonthPicker
